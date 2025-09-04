8d4fa1f05a70933f1a65de66350720e6d2215143
Eslam Linux <linuxeslam@gmail.com>
2025-09-04 14:36:59 +0000

    updated

---
+++ pointersandobjects.cpp
@@ -0,0 +1,36 @@
+using namespace std;
+#include <iostream>
+
+class tclass{
+    public:
+    int age;
+    string name;
+};
+tclass *ptr = new tclass;
+tclass *emp = new tclass;
+void prntptr(tclass *entry){
+cout << entry->name << "\t";
+cout << entry->age << "\n";
+}
+tclass* change_It(tclass* entry,string to_change_name = "unkown\n",int to_change_age =0){
+    tclass* newclass = new tclass;
+    newclass = entry;
+    newclass->name = to_change_name;
+    newclass->age = to_change_age;
+    return newclass;
+}
+int main(){
+
+ptr->name = "eslam khalid";
+ptr->age = 33;
+emp->name = "hassan khalid";
+emp->age = 28;
+
+
+prntptr(ptr);
+prntptr(change_It(ptr,"aya khalid",31));
+prntptr(ptr);
+prntptr(emp);
+
+    return 0;
+}
