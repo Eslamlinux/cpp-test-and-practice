6bcad520d0a0dc179fc949956cd607b60b65071e
Eslam Linux <linuxeslam@gmail.com>
2025-09-07 21:54:19 +0300

    Update linkedstack.cpp

--- linkedstack.cpp
+++ linkedstack.cpp
@@ -17,3 +17,19 @@ struct linkedlc{
     void insertBefore(char to_insert,char before_It);
     bool is_found(char to_find);
 };
+int main(){
+    linkedlc ls;
+    cout << boolalpha << ls.isEmpty() << noboolalpha << endl;
+    ls.insertNode('m');
+    ls.insertNode('a');
+    ls.insertNode('l');
+    ls.insertNode('s');
+    ls.insertNode('E');
+    cout << ls.count_words() << endl;
+    cout << endl;
+    cout << boolalpha << ls.isEmpty() << noboolalpha << endl;
+    cout << boolalpha << ls.is_found('a') << endl;
+    ls.display() ;
+
+    return 0;
+}
