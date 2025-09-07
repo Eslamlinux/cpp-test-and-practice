5a6c0fd2e428c5711ee65e4cb185cabb4b83932f
Eslam Linux <linuxeslam@gmail.com>
2025-09-07 21:56:18 +0300

    Create siml.cpp

---
+++ siml.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int main() {
+    vector<string> arr = {":)", ":D", ";-D", ":~)", ":~D", ":~"};
+    cout << countSmileys(arr) << endl; // Output: 5
+    return 0;
+}
