7ca6fbd2cc36625ec5c37580c370487db6fbb769
Eslam Linux <linuxeslam@gmail.com>
2025-09-07 21:56:33 +0300

    Update siml.cpp

--- siml.cpp
+++ siml.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+int countSmileys(vector<string> arr)
+{
+    int resultCount = 0;
+    for (string cur : arr) {
+        if (
+            (cur.length() == 2 &&
+                (cur[0] == ':' || cur[0] == ';') &&
+                (cur[1] == ')' || cur[1] == 'D'))
+            ||
+            (cur.length() == 3 &&
+                (cur[0] == ':' || cur[0] == ';') &&
+                (cur[1] == '-' || cur[1] == '~') &&
+                (cur[2] == ')' || cur[2] == 'D'))
+        ) {
+            resultCount++;
+        }
+    }
+    return resultCount;
+}
 
 
 int main() {
