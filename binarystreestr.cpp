b03ece5c130afd180c2531e3f143dc6ec9d7776a
Eslam Linux <linuxeslam@gmail.com>
2025-09-08 22:30:33 +0300

    Create binarystreestr.cpp

---
+++ binarystreestr.cpp
@@ -0,0 +1,16 @@
+using namespace std;
+#include <iostream>
+
+struct Node{
+    string data;
+    Node* right ,*left;
+    Node(string da){
+        right = left = NULL;
+        data = da;
+    }
+};
+
+int main(){
+
+    return 0;
+}
