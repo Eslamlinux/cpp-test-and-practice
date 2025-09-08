d23feb2f4b548d177a3ff8a2749b0e5bb5dc5a64
Eslam Linux <linuxeslam@gmail.com>
2025-09-08 22:32:44 +0300

    Update binarytreelinkedlist.cpp

--- binarytreelinkedlist.cpp
+++ binarytreelinkedlist.cpp
@@ -40,5 +40,52 @@ class Bst{
 };
 
 
+int main(){
 
+//45,15,79,90,10,55,12,20,50
+
+Bst bone;
+bone.insert(45);
+bone.insert(15);
+bone.insert(79);
+bone.insert(90);
+bone.insert(10);
+bone.insert(55);
+bone.insert(12);
+bone.insert(20);
+bone.insert(50);
+
+bone.preOrderd(bone.root);
+cout << "\n";
+bone.inOrderd(bone.root);
+cout << "\n";
+bone.postOrderd(bone.root);
+
+    return 0;
+}
+void Bst::preOrderd(Node* r){
+    if(r == NULL){
+        return;
+    }
+    cout << r->data << " ";
+    preOrderd(r->left);
+    preOrderd(r->right);
+}
+
+void Bst::inOrderd(Node* r){
+    if(r == NULL){
+        return;
+    }
+    inOrderd(r->left);
+    cout << r->data << " ";
+    inOrderd(r->right);
+}
+
+void Bst::postOrderd(Node* r){
+    if(r == NULL){
+        return;
+    }
+    postOrderd(r->left);
+    postOrderd(r->right);
+    cout << r->data << " ";
 }
