1c8472f8c3a32bd9426a1d04103e71c67de84a9c
Eslam Linux <linuxeslam@gmail.com>
2025-09-10 23:33:12 +0300

    Update filetest2.cpp

--- filetest2.cpp
+++ filetest2.cpp
@@ -2,10 +2,11 @@
 #include <fstream>
 void Creat_Txt_File(std::string entry)
 {
+
     std::ofstream myfile("filetest2.txt");
     myfile << entry << "\n";
     myfile.close();
-    std::fstream fileline("filetest2.txt");
+    std::ifstream fileline("filetest2.txt");
     std::string toread;
     while(getline(fileline ,toread)){
     std::cout << toread;
@@ -15,9 +16,7 @@ void Creat_Txt_File(std::string entry)
 int main()
 {
 Creat_Txt_File("eslam");
-
-
-
+Creat_Txt_File("hassan");
 
 
 
