f98b1b65482c0c3471e9f87c4ffb2ce165b832a7
Eslam Linux <linuxeslam@gmail.com>
2025-09-10 23:06:25 +0300

    Update filetest2.cpp

--- filetest2.cpp
+++ filetest2.cpp
@@ -2,6 +2,11 @@
 #include <fstream>
 void Creat_Txt_File(std::string entry)
 {
+    std::ofstream mufile("filetest2.bin");
+
+    std::fstream addto("filetest2.bin");
+
+    addto << "hello world\n";
   
 }
 
