083186f34dc571d3b60153b41cd14e1d3e1ee5be
Eslam Linux <linuxeslam@gmail.com>
2025-09-10 23:41:16 +0300

    Update filetest2.cpp

--- filetest2.cpp
+++ filetest2.cpp
@@ -2,10 +2,19 @@
 #include <fstream>
 void Creat_Txt_File(std::string entry)
 {
+    system("clear");
+
 
     std::ofstream myfile("filetest2.txt");
-    myfile << entry << "\n";
-    myfile.close();
+    myfile.open("filetest2.txt",std::ios::out|std::ios::app);
+    if(myfile.is_open()){
+        myfile << entry << "\n";
+        myfile.close();
+        system("clear");
+    }
+    else{
+        std::cerr << "Error: Could not open the file.\n";
+    }
     std::ifstream fileline("filetest2.txt");
     std::string toread;
     while(getline(fileline ,toread)){
