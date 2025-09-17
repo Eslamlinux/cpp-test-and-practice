9f26921fd2e1063271845511763d06190625ab37
Eslam Linux <linuxeslam@gmail.com>
2025-09-17 20:19:15 +0300

    Create ramadan.cpp

---
+++ ramadan.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+
+
+float calculateTotalFastingTime(vector<string> startTimes,vector<string> endTimes) { 
+float resault;
+vector<string> sTiH(startTimes);
+vector<string> eTiH(endTimes);
+
+
+
+
+int main() {
+
+
+
+    return 0;
+}
