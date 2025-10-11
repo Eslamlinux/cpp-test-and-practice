41a18d325a4feb945d0c68c88e27680ed0129e84
Eslam Linux <linuxeslam@gmail.com>
2025-10-11 21:24:58 +0000

    update tuple file

--- tuple1.cpp
+++ tuple1.cpp
@@ -5,10 +5,16 @@
 
 int main()
 {
-    std::tuple<int,std::string ,char> t1;
-    t1 = std::make_tuple(100 ,"hello" ,'E');
+    std::tuple<int,std::string ,char,std::string> t1;
+    t1 = std::make_tuple(100 ,"hello" ,'E',"slam");
     
 
+    int a = std::get<0>(t1);
+    std::string b = std::get<1>(t1);
+    char c = std::get<2>(t1);
+    auto d = std::get<3>(t1);
+
+    std::cout << a << " " << b << " " << c << "" << d << "\n";
 
     return 0;
 }
