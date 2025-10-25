67d522d3382bbe8930400632b9c7f6d51ef370c4
Eslamlinux <linuxeslam@gmail.com>
2025-10-25 23:44:06 +0300

    creat empty wx frame

---
+++ TheVowelCode.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+std::string encode(const std::string &str) {
+  return "";
+}
+
+std::string decode(const std::string &str) {
+  return "";
+}
+
+int main()
+{
+
+
+    return 0;
+}
