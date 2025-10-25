9ab3beb1d43f37199da8c6318a22e14ce1905b02
Eslamlinux <linuxeslam@gmail.com>
2025-10-26 00:57:36 +0300

    creat test file

--- TheVowelCode.cpp
+++ TheVowelCode.cpp
@@ -3,7 +3,7 @@
 
 std::string encode(const std::string &str) {
     std::string result = "";
-    std::string vowels = "aeiou"; //hello
+    std::string vowels = "aeiou"; 
     for(int i =0 ;i < str.size();i++){
         bool flag = false;
         for(int t = 0 ;t < vowels.size();t++){
@@ -12,8 +12,32 @@ std::string encode(const std::string &str) {
                 flag = true;
                 break;
             }
-            // else{
-                // }
+
+            }
+        if(!flag)
+            result+= str[i];
+        
+    }
+  return result;
+}
+std::string decode(const std::string &str) {
+    std::string result = "";
+    std::string vowels = "12345"; //hello
+    for(int i =0 ;i < str.size();i++){
+        bool flag = false;
+        for(int t = 0 ;t < vowels.size();t++){
+            if(tolower(str[i]) == vowels[t]){
+                switch (t){
+                    case 0: result+= 'a'; break;
+                    case 1: result+= 'e'; break;
+                    case 2: result+= 'i'; break;
+                    case 3: result+= 'o'; break;
+                    case 4: result+= 'u'; break;
+                }
+                flag = true;
+                break;
+            }
+
             }
         if(!flag){
             result+= str[i];
@@ -21,25 +45,6 @@ std::string encode(const std::string &str) {
     }
   return result;
 }
-std::string decode(const std::string &str) {
-    std::string result = "";
-    std::string vowels = "aeiou"; //hello
-    for(int i =0 ;i < str.size();i++){
-        bool flag = false;
-        for(int t = 0 ;t < vowels.size();t++){
-            if(tolower(str[i]) == vowels[t]){
-                result+= std::to_string(t+1);
-                flag = true;
-                break;
-            }
-            // else{
-                // }
-            }
-        if(!flag){
-            result+= str[i];
-        }
-    }
-  return result;}
 
 int main()
 {
@@ -51,9 +56,8 @@ i -> 3
 o -> 4
 u -> 5
 */
-
 std::cout << encode("hEllo") << std::endl;
-//std::cout << decode(h2ll4) << std::endl;
+std::cout << decode("h2ll4") << std::endl;
 
     return 0;
 }
