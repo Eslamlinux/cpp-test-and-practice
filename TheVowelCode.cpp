b307b19b71bdecedcddca72c85e9c2475dd536fd
Eslamlinux <linuxeslam@gmail.com>
2025-10-26 00:00:46 +0300

    creat test file

--- TheVowelCode.cpp
+++ TheVowelCode.cpp
@@ -5,7 +5,14 @@ std::string encode(const std::string &str) {
     std::string result = "";
     std::string vowels = "aeiou"
     for(int i =0 ;i < str.size();i++){
-        for(int t = 0 ;t < vowels.size();t++)
+        for(int t = 0 ;t < vowels.size();t++){
+            if(str[i] == vowels[t]){
+                result+= to_string(t+1);
+            }
+            else{
+                result+= str[i]
+            }
+        }
     }
   return result;
 }
@@ -26,7 +33,7 @@ u -> 5
 */
 
 std::cout << encode(hello) << std::endl;
-std::cout << decode(h2ll4) << std::endl;
+//std::cout << decode(h2ll4) << std::endl;
 
     return 0;
 }
