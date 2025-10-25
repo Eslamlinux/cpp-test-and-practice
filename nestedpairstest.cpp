233cc66cbc6d5484af2ced1f4dd15207c07eca5e
Eslamlinux <linuxeslam@gmail.com>
2025-10-26 02:08:49 +0300

    test nested pairs

--- nestedpairstest.cpp
+++ nestedpairstest.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
-
+#include <utility>
 
 int main()
 {
 
+    std::pair<std::string ,std::pair<int,std::pair<int,double>>> nestedpair;
+    nestedpair.first = "Eslam";
+    nestedpair.second.first = 0123550;
+    nestedpair.second.second.first = 34;
+    nestedpair.second.second.second = 173.5;
 
-
+    
     return 0;
 }
