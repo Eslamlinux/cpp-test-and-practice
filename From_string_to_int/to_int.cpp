c0258133c75cf4926c31b49027d7145359aa146d
Eslamlinux <linuxeslam@gmail.com>
2025-10-26 19:18:54 +0300

    implement function to convert string to integer

--- From_string_to_int/to_int.cpp
+++ From_string_to_int/to_int.cpp
@@ -1,3 +1,22 @@
-#include <iostream>
+#include "to_int.h"
 
+// Function to convert string to integer
 
+size_t to_int(const std::string &Enter_String_Has_A_Number) {
+    int result = 0;
+    for (char c : Enter_String_Has_A_Number) 
+    {
+        if(isdigit(c)) {
+        result = result * 10 + (c - '0');
+    }
+    }
+    return result;
+}
+
+// Example usage
+/*int main() {
+    std::string numberStr = "12345";
+    size_t number = to_int(numberStr);
+    std::cout << "The integer value is: " << number << std::endl;
+    return 0;
+}*/
