13e89e731289c674467eb7e625ae71f06e55f288
Eslamlinux <linuxeslam@gmail.com>
2025-10-28 00:04:40 +0300

    update

--- student_info/mainwindow.cpp
+++ student_info/mainwindow.cpp
@@ -2,12 +2,13 @@
 #include <iostream>
 #include <utility>
 #include <ctime>
+#include <string>
 
 class student
 {
 private:
-	int phone = 0
-	std::string birthday {""};
+	int phone = 0;
+	std::string birthDay {""};
 	int age = 0;
 public:
 	std::string name {""};
@@ -18,10 +19,10 @@ int main()
 {
 
 
+time_t day = time(nullptr);
 
 
-
-
+std::cout << ctime(&day) << std::endl;
 	return 0;
 }
 
