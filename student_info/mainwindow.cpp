c536fd85ca177b46d7a283635276630849385ccf
Eslamlinux <linuxeslam@gmail.com>
2025-10-27 23:48:26 +0300

    update

--- student_info/mainwindow.cpp
+++ student_info/mainwindow.cpp
@@ -1,9 +1,16 @@
 
 #include <iostream>
 #include <utility>
+#include <ctime>
 
 class student
 {
+private:
+	int phone = 0
+	std::string birthday {""};
+	int age = 0;
+public:
+	std::string name {""};
 
 };
 
