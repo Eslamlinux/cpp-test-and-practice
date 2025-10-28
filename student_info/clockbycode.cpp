38edbe55e0502d67d693bf11cca6640f2b74eedb
Eslamlinux <linuxeslam@gmail.com>
2025-10-29 00:28:13 +0300

    build Time clock

--- student_info/clockbycode.cpp
+++ student_info/clockbycode.cpp
@@ -5,8 +5,14 @@
 
 int main()
 {
+int h1 ,h2, m1, m2 ,s1 ,s2;
+std::cout << "enter hour 1" << std::endl;
+std::cin >>h1;
+std::cout << "enter hour 2" << std::endl;
+std::cin >>h2;
 
-std::pair<int,int> hour ={0,3};
+
+std::pair<int,int> hour ={h1,h2};
 std::pair<int,int> min ={5,9};
 std::pair<int,int> sec ={5,5};
 
@@ -14,15 +20,10 @@ std::string W_Time;
 
 while(true)
 {
-	system("clear");
-if(hour.first <= 1 && hour.second >=3)
-					{
-					W_Time = "PM";
-						}
-				if(hour.first < 1 && hour.second < 10 || hour.first <2 && hour.second < 2) 
-						{
-						W_Time = "AM";
-							}
+system("clear");
+
+//if(hour.first >= 1 && hour.second >=2){W_Time = "PM";}
+//if(hour.first <= 1 && hour.second <= 9) {W_Time = "AM";}
 
 if(sec.first >= 5 && sec.second  >= 9 )
 	{
@@ -33,14 +34,14 @@ if(sec.first >= 5 && sec.second  >= 9 )
 		{
 			min.first = 0;
 			min.second = 0;
-if(hour.first == 0 && hour.second == 0){hour.second++;}
-if(hour.first == 0 && hour.second == 9){hour.first++; hour.second = 0;}
-if(hour.first == 0 && hour.second < 9){hour.second ++;}
-if(hour.first == 1 && hour.second <2){hour.second++;}
-if(hour.first ==1 && hour.second == 2){hour.second++;}
-if(hour.first == 1 && hour.second > 2){hour.second++;}
-if(hour.first == 1 && hour.second == 9 ){hour.first++;hour.second =0;}
-if(hour.first == 2 && hour.second < 4){hour.second++;}
+if(hour.first == 0 && hour.second == 0){hour.second++; W_Time = "AM";}
+if(hour.first == 0 && hour.second < 9){hour.second ++; W_Time = "AM";}
+if(hour.first == 1 && hour.second == 9 ){hour.first++;hour.second = 0; W_Time = "PM";}
+if(hour.first == 1 && hour.second >= 2){hour.second++; W_Time = "PM";}
+if(hour.first == 1 && hour.second <2){hour.second++; W_Time = "PM";}
+if(hour.first == 0 && hour.second >= 9){hour.first++; hour.second = 0; W_Time = "AM";}
+if(hour.first == 2 && hour.second < 4){hour.second++; W_Time = "PM";}
+if(hour.first == 2 && hour.second == 4){hour.first = 0; hour.second = 0; W_Time = "PM";}
 
 		}
 	if(min.second < 9)
@@ -69,24 +70,6 @@ if(sec.second < 9 )
 		sec.first++;
 }
 		
-
-//	if(min > 59)
-//		{
-//			hour++;
-//			min = 00;
-//		}
-//	if(hour > 12)
-//		{
-//			W_Time = "Pm";
-//		}
-//		else 
-//			{
-//				W_Time = "Am";
-//			}
-//	if(hour >= 24)
-//			{
-//			hour = 00;	
-//			}
 
 	sleep(1);
 std::cout << "Time: " << hour.first << hour.second << " : " << min.first << min.second << " : " 
