951e9eeb1f9b08c7efe584be03b2b9fb89dbe596
Eslamlinux <linuxeslam@gmail.com>
2025-10-28 22:05:27 +0300

    build Time clock

--- student_info/clockbycode.cpp
+++ student_info/clockbycode.cpp
@@ -6,8 +6,10 @@
 int main()
 {
 
-int hour =23,min=50;
-std::pair<int,int> sec ={5,8};
+std::pair<int,int> hour ={0,0};
+std::pair<int,int> min ={0,0};
+std::pair<int,int> sec ={0,};
+
 std::string W_Time;
 
 while(true)
@@ -18,8 +20,42 @@ if(sec.first >= 5 && sec.second  >= 9 )
 	{
 	sec.first = 0;
 	sec.second = 0;
-	min++;
+
+	if(min.first >= 5 && min.second >= 9)
+		{
+			min.first = 0;
+			min.second = 0;
+				if(hour.first >= 2 && hour.second >= 4)
+					{
+						hour.first = 0; 
+						hour.second = 0;
+					}
+				if(hour.first <= 1 && hour.second >=3)
+					{
+					W_Time = "PM";
+						}
+					else 
+						{
+						W_Time = "AM";
+							}
+
+		}
+	if(min.second < 9)
+		{
+		min.second++;
+		}
+		else 
+		{
+		if(min.first < 6)
+		{
+		min.first++;
+		}
+		min.second =0;
+		}
+
 	}
+
+
 if(sec.second < 9 )
 	{
 	sec.second++;
