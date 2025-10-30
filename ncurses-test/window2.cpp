6af37aa8358db402ca783bd5d6d46a1c35ac835d
Eslamlinux <linuxeslam@gmail.com>
2025-10-30 23:33:20 +0200

    make new win with ncurses

--- ncurses-test/window2.cpp
+++ ncurses-test/window2.cpp
@@ -5,13 +5,61 @@ int main(int argc,char** argv)
 {
 
 
-start_ncurses(true,true);
+//start_ncurses(true,true);
+initscr();
+
+attron(A_INVIS);
+printw("password");
+attroff(A_INVIS);
 
 attron(A_REVERSE);
-printw("this is text");
-attroff(A_STANDOUT);
+printw("\nA REVERSE");
+attroff(A_REVERSE);
+
+attron(A_NORMAL);
+printw("\nA NORMAL");
+attroff(A_NORMAL);
+
+attron(A_BLINK);
+printw("\nA_BLINK");
+attroff(A_BLINK);
+
+attron(A_HORIZONTAL);
+printw("\n A_HORIZONTAL");
+attroff(A_HORIZONTAL);
+
+attron(A_BOLD);
+printw("\n A_BOLD");
+attroff(A_BOLD);
+
+attron(A_DIM);
+printw("\n A_DIM");
+attroff(A_DIM);
+
+attron(A_CHARTEXT);
+printw("\n A_CHARTEXT");
+attroff(A_CHARTEXT);
+
+attron(A_COLOR);
+printw("\n A_COLOR");
+attroff(A_COLOR);
+
+attron(A_PROTECT);
+printw("\n A_PROTECT");
+attroff(A_PROTECT);
+
+attron(A_ALTCHARSET);
+printw("\n A_ALTCHARSET");
+attroff(A_ALTCHARSET);
+
+attron(A_ATTRIBUTES);
+printw("\n A_ATTRIBUTES");
+attroff(A_ATTRIBUTES);
+
+
+
 
 getch();
-
+endwin();
 return 0;
 }
