8e2d862ef97da34466cc78253e4c71a0ac3e9794
Eslamlinux <linuxeslam@gmail.com>
2025-10-30 23:59:49 +0300

    modifire

--- ncurses-test/window2.cpp
+++ ncurses-test/window2.cpp
@@ -3,32 +3,35 @@
 
 int main(int argc,char** argv)
 {
-int h,w,x,y;
+//int h,w,x,y;
+//
+//initscr();
+//cbreak();
+////raw();
+//noecho();
+//
+//WINDOW* win = newwin(h = 15, w = 40, y = 5 , x = 5);
+//refresh();
+//
+//char t = '*';
+////box(win, (int)t, 104);
+////box(win, 0, 0);
+//int left, right, top, bottom, tlc, trc, blc, brc;
+//left = right = '$';
+//top = '#';
+//bottom = '#'; 
+//tlc = trc = blc = brc = (int)t;
+//wborder(win,left, right, top, bottom, tlc, trc, blc, brc);
+//mvwprintw(win, 1, 1,"hello window");
+//
+//wrefresh(win);
+//
+//int c = getch();
+//
+//endwin();
 
-initscr();
-cbreak();
-//raw();
-noecho();
+start_ncurses(true,true);
 
-WINDOW* win = newwin(h = 15, w = 40, y = 5 , x = 5);
-refresh();
-
-char t = '*';
-//box(win, (int)t, 104);
-//box(win, 0, 0);
-int left, right, top, bottom, tlc, trc, blc, brc;
-left = right = '$';
-top = '#';
-bottom = '#'; 
-tlc = trc = blc = brc = (int)t;
-wborder(win,left, right, top, bottom, tlc, trc, blc, brc);
-mvwprintw(win, 1, 1,"hello window");
-
-wrefresh(win);
-
-int c = getch();
-
-endwin();
 
 return 0;
 }
