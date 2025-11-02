8c5522adf046393edeb0f18d40003a5c5e3ad306
Eslamlinux <linuxeslam@gmail.com>
2025-11-02 23:44:09 +0200

    Find the unique number

--- finduniquenumber.cpp
+++ finduniquenumber.cpp
@@ -7,3 +7,18 @@ It’s guaranteed that array contains at least 3 numbers.
 
 The tests contain some very huge arrays, so think about performance
 */
+#include <iostream>
+#include <vector>
+
+float find_uniq(const std::vector<float> &v)
+{
+    return 0./.0;
+}
+
+int main()
+{
+
+std::cout << find_uniq(std::vector<float>{1, 1, 1, 2, 1, 1}) << std::endl;  // --> 2
+
+    return 0;
+}
