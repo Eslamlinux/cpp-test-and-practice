2f20b5821a30a2de534766b9129631f14acdc0fa
Eslamlinux <linuxeslam@gmail.com>
2025-11-03 21:10:07 +0200

    Sum of numbers from 0 to N

--- Sumofnumbersfrom0toN.cpp
+++ Sumofnumbersfrom0toN.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+/*
+Description:
 
+We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+Example:
+Input:
+
+> 6
+Output:
+
+0+1+2+3+4+5+6 = 21
+
+Input:
+
+> -15
+Output:
+
+-15<0
+
+Input:
+
+> 0
+Output:
+
+0=0
+*/
 
 class SequenceSum{
   int count;
@@ -10,8 +36,11 @@ class SequenceSum{
 };
 
 std::string SequenceSum::showSequence(){
-    //for
-      //while
+    if(count > 0)
+    {
+        
+    }
+    
 }
 
 SequenceSum::SequenceSum (int c) {
@@ -21,7 +50,8 @@ SequenceSum::SequenceSum (int c) {
 int main()
 {
 
-
+    SequenceSum seqsum (6);
+    seqsum.showSequence(); // Equals("0+1+2+3+4+5+6 = 21"));
 
 
     return 0;
