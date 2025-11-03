b11183f02a09428c28da46e6cf8e4385c2dfde4e
Eslamlinux <linuxeslam@gmail.com>
2025-11-03 21:52:31 +0200

    win3 implement

--- ncurses-test/window3.cpp
+++ ncurses-test/window3.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 #include <ncurses.h>
-
+#include <string>
 int main(int argc,  char** argv)
 {
 initscr();
 noecho();
 cbreak();
 
-int y, x, ybeg, xbeg, ymax, xmax);
+int y, x, ybeg, xbeg, ymax, xmax;
 getyx(stdscr, y, x);
 getbegyx(stdscr,ybeg ,xbeg);
 getmaxyx(stdscr,ymax ,xmax);
 
 
-
+printw("%d %d %d %d %d %d",y,x,ybeg,xbeg,ymax,xmax);
 
 return 0;
 }
