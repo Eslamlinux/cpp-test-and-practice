450d983823586c08e0eb55461e4f9d989b3229f8
Eslamlinux <linuxeslam@gmail.com>
2025-11-08 22:11:42 +0200

    manibleting

--- test/manibliating.cpp
+++ test/manibliating.cpp
@@ -1,5 +1,19 @@
 #include <iostream>
 
+/*
+std::endl;   <ostream>
+std::flush;  <ostream>
+std::setw(); <iomanip>
+std::left; std::right; std::internal;  <ios>
+std::setfill(); <iomanip>
+std::boolalpha; std::noboolaplpha; <ios>
+std::showpos; std::noshowpos;  <ios>
+std::dec; std::oct; std::hex; sts::showbase; std::noshowbase; std::uppercase; <ios>
+std::scientific; - std::fixed; <ios>
+std::setprecision; <iomanip>
+std::showpoint; std::noshopoint; <ios>
+*/
+
 int main()
 {
 	
