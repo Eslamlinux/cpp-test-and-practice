dd8981a778d772bb256473774386c59ce05e5fc3
Eslamlinux <linuxeslam@gmail.com>
2025-11-08 17:04:00 +0200

    switcherro

---
+++ test/Switcheroo.cpp
@@ -0,0 +1,23 @@
+/*
+Given a string made up of letters a, b, and/or c, switch the position of letters a and b (change a to b and vice versa). Leave any incidence of c untouched.
+
+Example:
+
+'acb' --> 'bca'
+'aabacbaa' --> 'bbabcabb'
+
+*/
+
+#include <string>
+#include <iostream>
+
+std::string switcheroo(const std::string &s) {
+  std::string result; 
+  
+  return "";
+}
+
+int main()
+{
+std::cout<<switcheroo("aaabcccbaaa") << std::endl;
+}
