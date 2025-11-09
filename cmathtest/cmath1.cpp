1e859190260cc22ce0270abff31dff66a2388fd7
Eslamlinux <linuxeslam@gmail.com>
2025-11-09 17:01:20 +0200

    cmath test

--- cmathtest/cmath1.cpp
+++ cmathtest/cmath1.cpp
@@ -19,5 +19,10 @@ std::cout << "sqrt: " << std::sqrt(10) << std::endl;
 std::cout << "pow: " <<std::pow(2,2) << std::endl;
 
 std::cout << "log10: " << std::log10(10000) << std::endl;
+std::cout <<"\nround test\n";
+std::cout << std::round(3.654) <<std::endl;
+std::cout << std::round(2.5) <<std::endl;
+std::cout <<std::round(2.4) << std::endl;
+
 return 0;
 }
