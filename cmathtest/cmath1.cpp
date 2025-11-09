65fa520380510d68a8efab4dee476f1e7a237a3d
Eslamlinux <linuxeslam@gmail.com>
2025-11-09 16:53:53 +0200

    cmath test

--- cmathtest/cmath1.cpp
+++ cmathtest/cmath1.cpp
@@ -3,13 +3,21 @@
 
 int main()
 {
-double weight {7.7};
+double weight {7.5};
 double savings {-5000};
+std::cout << "abs test\n";
 std::cout << abs(savings) << std::endl;
 std::cout << abs(weight) << std::endl;
-
+std::cout << "\nceil and floor test\n";
 std::cout << ceil(weight) << std::endl;
 std::cout << floor(weight) << std::endl;
 
+double exptional = std::exp(10);
+std::cout << std::endl << "exptional: " << exptional << std::endl; 
+std::cout << "sqrt: " << std::sqrt(10) << std::endl;
+
+std::cout << "pow: " <<std::pow(2,2) << std::endl;
+
+std::cout << "log10: " << std::log10(10000) << std::endl;
 return 0;
 }
