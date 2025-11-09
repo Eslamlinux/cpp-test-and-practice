75f9bab0f6f98af812424dddf1a74379d3ce102b
Eslamlinux <linuxeslam@gmail.com>
2025-11-09 14:45:27 +0200

    limits practes

--- limits/limits1.cpp
+++ limits/limits1.cpp
@@ -4,12 +4,15 @@
 
 int main()
 {
-std::cout << "short: " << std::numeric_limits<short>::min() << std::numeric_limits<short>::max() << std::endl;
-std::cout <<"unsignd short: " << std::numeric_limits<unsigned short>::min() << std::numeric_limits<unsigned short>::max() <<std::endl;
-std::cout << "long: " << std::numeric_limits<long>::min() << std::numeric_limits<long>::max() << std::endl;
-std::cout << "int: " << std::numeric_limits<int>::min() << std::numeric_limits<int>::max() << std::endl;
-
-std::cout << "float: " << std::numeric_limits<float>::min() << std::numeric_limits<float>::max() << std::endl;
-std:: cout << "lowes float: " << std::numeric_limits<float>::lowest() << std::numeric_limits<float>::max() << std::endl;
-std::cout << "double: " <<std::numeric_limits<double>::min() <<std::numeric_limits<double>::max() << std::endl;
+std::cout << "short from: " << std::numeric_limits<short>::min() << " to: "<< std::numeric_limits<short>::max() << std::endl;
+std::cout <<"unsignd short: " << std::numeric_limits<unsigned short>::min() << " to: " << std::numeric_limits<unsigned short>::max() <<std::endl;
+std::cout << "long: " << std::numeric_limits<long>::min() << " to: " << std::numeric_limits<long>::max() << std::endl;
+std::cout << "int: " << std::numeric_limits<int>::min() << " to: " << std::numeric_limits<int>::max() << std::endl;
+std::cout << "unsigned int: " << std::numeric_limits<unsigned int>::min() << " to: " << std::numeric_limits<unsigned int>::max() << std::endl;
+std::cout << "float: " << std::numeric_limits<float>::min() << " to: " << std::numeric_limits<float>::max() << std::endl;
+std:: cout << "lowes float: " << std::numeric_limits<float>::lowest() << " to: " << std::numeric_limits<float>::max() << std::endl;
+std::cout << "double: " <<std::numeric_limits<double>::min()<< " to: " <<std::numeric_limits<double>::max() << std::endl;
+std::cout << "lowest double: " <<std::numeric_limits<double>::lowest()<< " to: " <<std::numeric_limits<double>::max() << std::endl;
+std::cout << "long double: " <<std::numeric_limits<long double>::min()<< " to: " <<std::numeric_limits<long double>::max() << std::endl;
+std::cout << "lowest long double: " <<std::numeric_limits<long double>::lowest()<< " to: " <<std::numeric_limits<long double>::max() << std::endl;
 }
