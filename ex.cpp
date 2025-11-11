5d51e495fbac14df7f46ebb82634f837628dedc4
Eslamlinux <linuxeslam@gmail.com>
2025-11-11 18:14:32 +0200

    >> <<

--- ex.cpp
+++ ex.cpp
@@ -1,7 +1,7 @@
 #include <iostream> 
 int main() {
      int x = 10; 
-     int y = 4; 
-     int z = x >> y;
+     int y = 2; 
+     int z = x << y;
     std::cout << z << std::endl; 
      return 0; }
