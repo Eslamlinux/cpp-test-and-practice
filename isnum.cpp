47760defabdf8b90425c47f98d02c4aeccdd53f7
Eslamlinux <linuxeslam@gmail.com>
2025-11-11 16:00:12 +0200

    is all Same Case

---
+++ isnum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+bool isNumber(string s) { 
+    for (char c : s) {
+                if(c == '+') continue ;
+                if( c == '.')  continue;
+                if(c == 'e') continue ;
+                if(c == '-') continue ;
+       return !isdigit(c)? false : true;
+    }   
+return true;
+}
+int main() {
+    string testStr = "+123.456e789";
+    if (isNumber(testStr)) {
+        cout << testStr << " is a number." << endl;
+    } else {
+        cout << testStr << " is not a number." << endl;
+    }
+    return 0;
+}
