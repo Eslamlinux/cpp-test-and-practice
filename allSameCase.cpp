9f1b82c96c126e5186543854a7557e42d6fe11d3
Eslamlinux <linuxeslam@gmail.com>
2025-11-11 16:26:12 +0200

    solve all same case

--- allSameCase.cpp
+++ allSameCase.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 using namespace std;
 
+// bool allSameCase(string word) { 
+//     bool hasLower = false;
+//     bool hasUpper = false;
+//     for (char c : word) {
+//         if (islower(c)) hasLower = true;
+//         if (isupper(c)) hasUpper = true;
+//         if (hasLower && hasUpper) return false;
+//     }
+//     return true;
+// }
+
+
 bool allSameCase(string word) { 
-    bool hasLower = false;
-    bool hasUpper = false;
+    bool hasLower = false, hasUpper = false;
     for (char c : word) {
-        if (islower(c)) hasLower = true;
-        if (isupper(c)) hasUpper = true;
-        if (hasLower && hasUpper) return false;
+        islower(c)? hasLower = true: hasUpper = true ;
+        isupper(c)? hasUpper = true : hasLower = true ;
+        if(hasLower && hasUpper) return false;
     }
     return true;
 }
