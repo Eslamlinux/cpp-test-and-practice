66c23d68d54d820ae274a91cf215bfa205695077
Eslamlinux <linuxeslam@gmail.com>
2025-11-11 16:55:31 +0200

    count down solve

--- countdown.cpp
+++ countdown.cpp
@@ -7,7 +7,6 @@ vector<int> countdown(int num) {
  vector<int> result;
  if(num <= 3 ){return {0};}
  for(int i = num; i > 1 ; i = i -3){
-    // int temp = i - 3;
     if(i % 2 == 0 && i != num){
         result.push_back(i);
     }
@@ -18,8 +17,8 @@ vector<int> countdown(int num) {
 
 int main() {
 
-for(int c: countdown(10)){
-    cout << c << endl;
+for(int c: countdown(103)){
+    cout << c << " ";
 }
 
   return 0;
