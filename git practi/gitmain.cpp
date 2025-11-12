1d909b46f8f1e7e010e2b4f2ffba55ab6cd4c95b
Eslamlinux <linuxeslam@gmail.com>
2025-11-12 21:23:21 +0200

    creat first file

--- git practi/gitmain.cpp
+++ git practi/gitmain.cpp
@@ -1,10 +1,38 @@
-/*
- * gitmain.cpp
- *
- *  Created on: Nov 12, 2025
- *      Author: eslam
- */
+#include <iostream>
+
+struct user
+{
+	std::string name = "demo\n";
+	std::string email = "not set\n";
+	std::string passwd = "******";
 
 
+};
 
+void UserAct()
+{
+	user user;
+	std::string entry;
+	std::cout  << " please chose\n";
+	std::cout  << " to login press l\n";
+	std::cout  << " add user account to git press u\n";
+	if(entry == "u")
+	{
+	std::cout << user.name << " please enter your user name\n";
+	std::cin >> user.name;
+	std::cout << user.name << " please add your email\n";
+	std::cin >> user.email;
+	std::cout << user.name << " please add your password\n";
+	std::cin >> user.passwd;
+	}
 
+}
+
+int main()
+{
+while (true) 
+{
+	UserAct();
+}
+return 0;
+}
