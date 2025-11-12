304c673cee3512eb7229b8b7ed2bf59fd096a291
Eslamlinux <linuxeslam@gmail.com>
2025-11-12 10:03:43 +0200

    solve maximumproduct

--- MaximumProduct.cpp
+++ MaximumProduct.cpp
@@ -1,18 +1,21 @@
+#include <iostream>
 #include <vector>
 
 
-int adjacentElementsProduct(vector<int> inputArray) 
+int adjacentElementsProduct(std::vector<int> inputArray) 
   {
-  int res = inutArray[0] * inputArray[1];
-  for(int i = 0;i < inputArray.size() -2;i++)
+  int res = inputArray[0] * inputArray[1];
+  for(int i = 0;i < inputArray.size() -1;i++)
     {
-    if(inutArray[i] * inputArray[i+1] > res)
-    res = inutArray[i] * inputArray[i+1];
+    if(inputArray[i] * inputArray[i+1] > res)
+    res = inputArray[i] * inputArray[i+1];
     }
   return res ;
   }
   int main()
   {
+    std::vector<int> v = {3, 6, -2, -5, 7, 3};
+    std::cout << adjacentElementsProduct(v);
   
     return 0;
   }
