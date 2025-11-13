0d4546c42de0da379c72e22f5dbfe7a75078456c
Eslamlinux <linuxeslam@gmail.com>
2025-11-13 13:15:01 +0200

    make a move game

--- bardi/begin3.cpp
+++ bardi/begin3.cpp
@@ -14,7 +14,7 @@ while(key != 'q')
 	{
 		clear();
 		move(0,0);
-		printw("welcome to first move game program '%d");
+		printw("welcome to first move game program to quit press 'q'");
 		
 		move(y,x);
 		printw("-");
@@ -23,10 +23,24 @@ while(key != 'q')
 		key = getch();
 		if(key == KEY_LEFT)
 			{
-				x++;
-			if(x < 0) x=0;
+				x--;
+				if(x < 0) x = 0;
 			}
-
+		if(key == KEY_RIGHT)
+			{
+				x++;
+				if(x > 30) x = 30;			
+			}
+		if(key == KEY_UP)
+			{
+				y--;
+				if(y < 0) y=0;
+			}
+		if(key == KEY_DOWN)
+			{
+				y++;
+				if(x > 30) x = 30;
+			}
 	}
 endwin();
 return 0;
