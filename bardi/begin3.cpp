c4550ef97ca222455917e02a599742cec8d6b1c8
Eslamlinux <linuxeslam@gmail.com>
2025-11-13 12:59:11 +0200

    make a move game

--- bardi/begin3.cpp
+++ bardi/begin3.cpp
@@ -1,8 +1,33 @@
+#include <ios>
 #include <iostream>
 #include <ncurses.h>
 
 int main()
 {
-	
+	initscr();
+int x,y,key;
+keypad(stdscr,true);
+noecho();
+x = y =5;
 
+while(key != 'q')
+	{
+		clear();
+		move(0,0);
+		printw("welcome to first move game program '%d");
+		
+		move(y,x);
+		printw("-");
+		refresh();
+		
+		key = getch();
+		if(key == KEY_LEFT)
+			{
+				x++;
+			if(x < 0) x=0;
+			}
+
+	}
+endwin();
+return 0;
 }
