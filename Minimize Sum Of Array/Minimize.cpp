2fa724917daaaf2ae4ba556f093b622a2f0aecdc
Eslamlinux <linuxeslam@gmail.com>
2025-11-16 17:46:31 +0200

    Minimize Sum Of Array

--- Minimize Sum Of Array/Minimize.cpp
+++ Minimize Sum Of Array/Minimize.cpp
@@ -1,10 +1,24 @@
 /*
- * Minimize.cpp
- *
- *  Created on: Nov 16, 2025
- *      Author: eslam
+ Given an array of integers , Find the minimum sum which is obtained from summing each Two integers product .
+
+Notes
+Array/list will contain positives only .
+Array/list will always have even size
+Input >> Output Examples
+minSum({5,4,2,3}) ==> return (22)
+Explanation:
+The minimum sum obtained from summing each two integers product ,  5*2 + 3*4 = 22
  */
+#include <iostream>
+#include <vector>
 
+int minSum(const std::vector<int> &passed)
+{
+    // your Code Here .... hope you Enjoy !!!
+    return 0;
+}
 
-
-
+int main()
+{
+	return 0;
+}
