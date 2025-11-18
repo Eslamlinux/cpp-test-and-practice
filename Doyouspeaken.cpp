d87deadec58f83279d8b5b369f24fb28754c6547
Eslam Linux <linuxeslam@gmail.com>
2025-11-18 23:22:08 +0200

    Add basic English sentence processing program

---
+++ Doyouspeaken.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+bool sp_eng(const std::string& sentence) {
+    std::string low = std::tolower(sentence);
+    std::cout << low << std::endl;
+
+    return true;
+}
+
+int main() {
+    std::cout << sp_eng("1234#$%%eNglish ;k9") << std::endl;
+    return 0;
+}
