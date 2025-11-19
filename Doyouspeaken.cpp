67a800c50fc2119c20cc72dcbde100622b44986e
Eslamlinux <linuxeslam@gmail.com>
2025-11-19 23:46:53 +0200

    do you speake en solve

--- Doyouspeaken.cpp
+++ Doyouspeaken.cpp
@@ -14,15 +14,11 @@ Return value as boolean values, true for the string to contains "English", false
 #include <algorithm>
 
 bool sp_eng(const std::string& sentence) {
-    std::string tolow = std::tolower(sentence);
+    std::string tolow = sentence ;
+    std::transform(tolow.begin(),tolow.end(),tolow.begin(),[](unsigned char c){return std::tolower(c);});
     size_t low = sentence.find("english");
-    if(low == std::string::npos)
-    std::cout << "not find it" << std::endl;
-    else
-    std::cout << "find it" << std::endl;
-
-
-    return true;
+    std::cout << std::boolalpha;
+    return low != std::string::npos? false:low;
 }
 
 int main() {
