2d66c1f68114ca03d2c82b6bfbfb964121f28879
Eslamlinux <linuxeslam@gmail.com>
2025-11-22 17:02:36 +0200

    binary to decimal or decimal to binary test

--- binarydec.cpp
+++ binarydec.cpp
@@ -3,14 +3,18 @@
 #include <vector>
 #include <algorithm>
 #include <cmath>
-std::vector<int> ToBinary(int& n)
+std::vector<int> To_Binary(std::string& str)
 {
     std::vector<int>result;
-
-    while(n > 0)
+        for(char c :str)
     {
-        n % 2 == 0?result.insert(result.begin(),0):result.insert(result.begin(),1);
-        n /= 2;
+        if(isalpha(c)) break;
+    }
+    int Decimal = stoi(str);
+    while(Decimal > 0)
+    {
+        Decimal % 2 == 0?result.insert(result.begin(),0):result.insert(result.begin(),1);
+        Decimal /= 2;
     } 
     return result;
 }
@@ -31,16 +35,41 @@ int To_Decimal(std::string str)
     return sum;
 }
 
+void Get_entry(std::string str)
+{
+    std::cout << "please chose wht you want to confert from " << std::endl;
+    std::cout << "decimal to binary chose [1] " << std::endl;
+    std::cout << "binary to decimal chose [2] " << std::endl;
+    int chose;
+    std::string entry;
+    std::cin >> chose;
+    if(chose == 1)
+    {
+    std::cout << "please enter decimal to convert: " ;
+    std::cin >> entry;
+    for(auto c:To_Binary(entry))
+    {
+            std::cout << c << "" ;
+    }
+    }
+    else if(chose == 2)
+    {
+    std::cout << "please enter binary to convert: " ;
+    std::cin >> entry;
+    std::cout << To_Decimal(entry);
+    }
+
+}
 int main()
 {
-    // int ent;
-    // std::cin >>ent;
-    // for(auto c:ToBinary(ent))
-    // {
-        //     std::cout << c << "" ;
-        // }
-        std::string ent;
-std::cin >> ent;
+    std::string ent;
+    std::cin >>ent;
+    for(auto c:ToBinary(ent))
+    {
+            std::cout << c << "" ;
+    }
+//         std::string ent;
+// std::cin >> ent;
 std::cout << To_Decimal(ent);
     return 0;
 }
