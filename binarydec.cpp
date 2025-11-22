74addbf344c3fe1e231033a4dc63a7f381946fab
Eslamlinux <linuxeslam@gmail.com>
2025-11-22 18:58:33 +0200

    binary to decimal or decimal to binary test

--- binarydec.cpp
+++ binarydec.cpp
@@ -40,30 +40,39 @@ void Get_entry()
     std::cout << "please chose what do you want to convert from " << std::endl;
     std::cout << "decimal to binary chose [1] " << std::endl;
     std::cout << "binary to decimal chose [2] " << std::endl;
-    int chose;
+    std::cout << "Exit chose [3] " << std::endl;
+    char chose;
     std::string entry;
     std::cin >> chose;
-    if(chose == 1)
+    if(chose == '1')
     {
         system("clear");
     std::cout << "please enter decimal to convert: " ;
     std::cin >> entry;
+    std::cout << "you convert frome "<< entry << " To "  ;
     for(auto c:To_Binary(entry))
     {
-            std::cout << "you convert frome "<< entry << " To " <<  c << " binary\n" ;
+        std::cout << c;
     }
+        std::cout <<" binary\n" ;
     }
-    else if(chose == 2)
+    else if(chose == '2')
     {
+        system("clear");
     std::cout << "please enter binary to convert: " ;
     std::cin >> entry;
     std::cout << "you convert frome "<< entry << " To " << To_Decimal(entry) << " decimal\n";
     }
-
+    else 
+    return;
 }
 int main()
 {
-    Get_entry();
+    while (true)
+    {
+        Get_entry();
+    }
+    
  
     return 0;
 }
