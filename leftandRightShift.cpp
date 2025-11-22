ce9f8c1e689679388bdd29b5d22d7db5ea51492a
Eslamlinux <linuxeslam@gmail.com>
2025-11-22 23:32:18 +0200

    test left and right shifting

--- leftandRightShift.cpp
+++ leftandRightShift.cpp
@@ -5,8 +5,17 @@ int main()
 {
     std::cout << To_Binary_dir("9") << std::endl; // 1001
     std::cout << To_Decimal("100100") << std::endl; // 36 after do << left shift
-    std::cout << (9 << 2) << std::endl;
-    std::cout << (36 >> 2) << std::endl;
+    std::cout << (9 << 1) << std::endl;
+    std::cout << (36 >> 1) << std::endl;
+    int a = 10;
+    int b = 20;
 
+    std::cout << "befor swap: " << (a) << " " << b << std::endl;
+
+    a = a ^ b;
+    b = a ^ b;
+    a = a ^ b;
+    std::cout << "after swap by bits: " << (a) << " " << b << std::endl;
+    
     return 0;
 }
