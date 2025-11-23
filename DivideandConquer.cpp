d28e3a56be8ff2e4bc0f869da13978a50ca3a1a1
Eslamlinux <linuxeslam@gmail.com>
2025-11-23 23:24:30 +0200

    Divide and Conquer solve problem

--- DivideandConquer.cpp
+++ DivideandConquer.cpp
@@ -1,5 +1,17 @@
+/*
+Given a mixed array of number and string representations of integers, add up the non-string integers and subtract the total of the string integers.
+
+Return as a number.
+*/
 #include<iostream>
 #include <variant>
+#include <vector>
+#include <variant>
+
+int divCon(const std::vector<std::variant<int, char>>& x) {
+  //your code here
+  return 0;
+}
 
 int main()
 {
