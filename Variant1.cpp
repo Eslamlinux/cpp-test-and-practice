edb88062c8429fe86df62f98e0076c14d096ad4e
Eslamlinux <linuxeslam@gmail.com>
2025-11-24 00:35:25 +0200

    multiplying

--- Variant1.cpp
+++ Variant1.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include <variant>
 
+
+
 int main()
 {
     std::variant<std::string,int> v1;
     v1 = "hello";
-
-    std::cout << std::get_if<std::string>(v1) << std::endl;
+    v1 = 5;
+    if(std::get_if<std::string>(&v1))
+    std::cout << std::get<std::string>(v1) << std::endl;
+    else
+    std::cout << std::get<int>(v1) << std::endl;
 
     return 0;
 }
