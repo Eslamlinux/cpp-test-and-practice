3870f13da8578f134c3caee10d71c53006da455e
Eslamlinux <linuxeslam@gmail.com>
2025-11-24 01:07:19 +0200

    multiplying

--- Variant1.cpp
+++ Variant1.cpp
@@ -2,16 +2,17 @@
 #include <variant>
 #include <string>
 
-std::variant<std::string,char, int,double,float> func(std::string str)
+std::variant<std::string,char, int,double> func(std::string str)
 {
     if(str == "st")
-    return  "hello this is string return\n";
+    return  "hello this is string return";
     else if(str == "in")
     return 123456;
     else if(str == "fl")
     return 1.22;
-    else
+    else if(str == "do")
     return 1.23456;
+    else return 1;
 
 
     // return str == "st" ? "hello this is string return" :
@@ -21,13 +22,17 @@ std::variant<std::string,char, int,double,float> func(std::string str)
 
 int main()
 {
-    std::variant<std::string,int> v1;
-    v1 = "hello";
-    v1 = 5;
-    if(std::get_if<std::string>(&v1))
-    std::cout << std::get<std::string>(v1) << std::endl;
-    else
-    std::cout << std::get<int>(v1) << std::endl;
+    // std::variant<std::string,int> v1;
+    // v1 = "hello";
+    // v1 = 5;
+    // if(std::get_if<std::string>(&v1))
+    // std::cout << std::get<std::string>(v1) << std::endl;
+    // else
+    // std::cout << std::get<int>(v1) << std::endl;
 
+        std::cout<< std::get<std::string>(func("st")) <<std::endl;
+        std::cout<< std::get<int>(func("in")) <<std::endl;
+        std::cout<< std::get<double>(func("fl")) <<std::endl;
+        std::cout<< std::get<double>(func("do")) <<std::endl;
     return 0;
 }
