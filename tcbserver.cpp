8a2c3f77d775313d8fa29953669b40a1e612e60e
Eslamlinux <linuxeslam@gmail.com>
2025-11-24 22:32:17 +0200

    make Tcp server

--- tcbserver.cpp
+++ tcbserver.cpp
@@ -2,6 +2,24 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+static void die(char* m)
+{
+    perror(m);
+    exit(1);
+}
 
-
-
+int main(void)
+{
+    int s = socket(AF_INET,SOCK_STREAM,0);
+    if(s < 0) 
+    die("socket");
+}
+int opt = 1;
+if(setsockopt(s,SOL_SOCKET,SO_REUSEADDR,&opt,sizeof(opt))<0)
+{
+    die("setsocket");
+}
+struct sockaddr_in a = {0};
+a.sin_falmily = AF_INET;
+a.sin_addr.s_addr = htonl(INADDR_ANY);
+a.sin_port = htons(8080);
