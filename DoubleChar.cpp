9302844d8723c186179b68244540afd7cb03357b
Eslamlinux <linuxeslam@gmail.com>
2025-11-25 00:38:02 +0200

    Double Char

--- DoubleChar.cpp
+++ DoubleChar.cpp
@@ -1,7 +1,19 @@
+/*
+Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+Examples (Input -> Output):
+* "String"      -> "SSttrriinngg"
+* "Hello World" -> "HHeelllloo  WWoorrlldd"
+* "1234!_ "     -> "11223344!!__  "
+Good Luck!
+
+
+*/
 #include <iostream>
 
 int main()
 {
+
 
 
     return 0;
