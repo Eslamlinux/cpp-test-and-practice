893c42c3a55f97e42953356a513f6efd89edaae0
Eslamlinux <linuxeslam@gmail.com>
2025-11-24 04:35:47 +0200

    union test

--- union1.cpp
+++ union1.cpp
@@ -5,12 +5,19 @@ union sam
 private:
 int passcode {123};
 public:
-int num {1};
-std::string str{"hello"};
-
+int num;
+std::string str;
+int get_code()
+{
+    return passcode;
+}
+char c;
 };
 int main()
 {
+    
+    std::cout << sam::get_code();
+
 
     return 0;
 }
