64b7b281d12cb44ebb138aa3a150b9d3fc1d6838
Eslamlinux <linuxeslam@gmail.com>
2025-11-24 06:07:53 +0200

    union test

--- union1.cpp
+++ union1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <string>
+#include <cstring>
 
 union sam
 {
@@ -29,18 +29,19 @@ union sam
 int main()
 {
     sam fir;
-    fir.c[0] = 'e';
-    fir.c[1] = 's';
-    fir.c[2] = 'l';
-    fir.c[3] = 'a';
-    fir.c[4] = 'm';
-    fir.c[5] = ' ';
-    fir.c[6] = 'k';
-    fir.c[7] = 'h';
-    fir.c[8] = 'a';
-    fir.c[9] = 'l';
-    fir.c[10] = 'i';
-    fir.c[11] = 'd';
+    // fir.c[0] = 'e';
+    // fir.c[1] = 's';
+    // fir.c[2] = 'l';
+    // fir.c[3] = 'a';
+    // fir.c[4] = 'm';
+    // fir.c[5] = ' ';
+    // fir.c[6] = 'k';
+    // fir.c[7] = 'h';
+    // fir.c[8] = 'a';
+    // fir.c[9] = 'l';
+    // fir.c[10] = 'i';
+    // fir.c[11] = 'd';
+    strcpy(fir.c,"eslam khalid");
     fir.printc() ;
     std::cout << "\n";
     std::cout << "the top data size: " << sizeof(fir) <<std::endl;
