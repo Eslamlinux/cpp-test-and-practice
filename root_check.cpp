186c5041aca77cba46a513f858f5c08259de839a
Eslamlinux <linuxeslam@gmail.com>
2025-11-25 20:51:40 +0200

    root check

--- root_check.cpp
+++ root_check.cpp
@@ -3,14 +3,17 @@
 
 
 int root_check(float sqr,int num) { 
-return sqrt(sqr) == num?sqr:0;
+return sqrt(sqr) == num? num:0;
 }
 
 int main()
 {
 
-    std::cout << root_check(25,5);
+    std::cout << root_check(25,5) << std::endl;
+    std::cout << root_check(4,2) << std::endl;
+    std::cout << root_check(49,8) << std::endl;
+    std::cout << root_check(64,8) << std::endl;
     std::cout << "\n";
-    std::cout << sqrt(25);
+    std::cout << sqrt(64);
     return 0;
 }
