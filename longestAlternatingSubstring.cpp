630b5b0c36385c949dde5e15c7420e349fa74871
Eslamlinux <linuxeslam@gmail.com>
2025-11-25 22:01:47 +0200

    longestAlternatingSubstring

---
+++ longestAlternatingSubstring.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+std::string longestAlternatingSubstring(std::string digits) { 
+
+}
+
+int main()
+{
+    std::cout << longestAlternatingSubstring("2105787220351146") << std::endl;
+    return 0; 
+}
