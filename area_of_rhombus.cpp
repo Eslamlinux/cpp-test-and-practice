0a7ee8b5a42406fa58b0e58b32d7ddbfd9a420e7
Eslamlinux <linuxeslam@gmail.com>
2025-11-25 16:57:57 +0200

    area of rhombus

--- area_of_rhombus.cpp
+++ area_of_rhombus.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
 
+float area_of_rhombus(float q,float p) { 
+float result = (q * p) / 2;
+return result;
+}
+
+
 int main()
 {
-
+std::cout << area_of_rhombus(9.2,8.6) << std::endl;
     return 0;
 }
