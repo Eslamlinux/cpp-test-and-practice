9b8bbf1c7f5133dde306f33d0de0dd1f16857818
Eslamlinux <linuxeslam@gmail.com>
2025-11-26 22:23:51 +0200

    is mirrored

--- isMirrored.cpp
+++ isMirrored.cpp
@@ -7,10 +7,11 @@ bool isMirrored(int num) {
         if(str[i] != str[str.size() - 1 -i])
         return false;
     }
+    return true;
 }
 
 int main()
 {
-    std::cout << isMirrored(11) << std::endl;
+    std::cout << isMirrored(121) << std::endl;
     return 0;
 }
