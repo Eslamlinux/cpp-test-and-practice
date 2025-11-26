e1fa8a36abd8311232cc1d209d14281c75749c02
Eslamlinux <linuxeslam@gmail.com>
2025-11-26 22:21:39 +0200

    isMirrored

--- isMirrored.cpp
+++ isMirrored.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
 bool isMirrored(int num) { 
-    
+    std::string str = std::to_string(num);
+    for(int i = 0; i < str.size() /2;i++)
+    {
+        if(str[i] != str[str.size() - 1 -i])
+        return false;
+    }
 }
 
 int main()
