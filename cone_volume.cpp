b5dff5e5358ab4387ad68f271e1d00c85582506b
Eslamlinux <linuxeslam@gmail.com>
2025-11-30 17:56:44 +0200

    get biggest shared

--- cone_volume.cpp
+++ cone_volume.cpp
@@ -2,12 +2,13 @@
 #include <cmath>
 
 float cone_volume(float radius,float height) { 
-    return (pow(radius,2)*1/3) * 3.14159 * height;
+    return (pow(radius,2)*1/3) * 3.14 * height;
 }
 
 int main()
 {
 std::cout << cone_volume(1.3,5.2) << std::endl;
+std::cout << cone_volume(8.7,7.8) << std::endl;
 
     return 0;
 }
---
+++ getBiggestShared.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <set>
+#include <vector>
+int getBiggestShared(std::vector<int> a,std::vector<int> b) { 
+// write your code here ^_^
+}
