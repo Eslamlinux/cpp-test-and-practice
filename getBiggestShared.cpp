0eba1317cbd6c9c98d48e268fc9f90d63b5d6feb
Eslamlinux <linuxeslam@gmail.com>
2025-11-30 19:48:03 +0200

    cone_volume

--- getBiggestShared.cpp
+++ getBiggestShared.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
 #include <set>
+// #include <bits/stdc++.h>
 #include <vector>
 int getBiggestShared(std::vector<int> a,std::vector<int> b) { 
-// write your code here ^_^
+    std::set<int , std::greater<int>> firstarr(a.begin(),a.end());
+    int result =0;
+    std::set<int, std::greater<int>>::iterator it;
+    for(int c : b)
+    {
+        auto myf = firstarr.find(c);
+        if(*myf > result )
+        result = c;
+    }
+    return result;
 }
 
 int main()
 {
     std::cout << getBiggestShared({90, 95},{90, 95}) << std::endl;
-    
+
     return 0;
 }
