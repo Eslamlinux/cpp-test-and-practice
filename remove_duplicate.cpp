840420a3b32491283318d3572075a75b64f0edcf
Eslamlinux <linuxeslam@gmail.com>
2025-12-02 06:45:17 +0200

    remove_duplicate

--- remove_duplicate.cpp
+++ remove_duplicate.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+#include <unordered_set>
 #include <set>
 #include <vector>
 std::vector<int> remove_duplicate(std::vector<int> arr) { 
-std::set<int> tosets(arr.begin(),arr.end());
-std::vector<int> result(tosets.begin(),tosets.end());
-return result;
+std::set<int> toset(arr.begin(),arr.end());
+return {toset.begin(),toset.end()};
+
+// std::set<int> tosets(arr.begin(),arr.end());
+// std::vector<int> result(tosets.begin(),tosets.end());
+// return result;
+
+// std::unordered_set<int> toset(arr.begin(),arr.end());
+// return {toset.begin(),toset.end()};
 }
 int main()
 {
-    for(auto c : remove_duplicate({7, 8, 9, 7}))
+    for(auto c : remove_duplicate({7,8,9,7}))
+    {
+        std::cout << c << " ";
+    }
+    std::cout << std::endl;
+        for(auto c : remove_duplicate({7,8,9,7}))
     {
         std::cout << c << " ";
     }
