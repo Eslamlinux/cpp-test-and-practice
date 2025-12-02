490097068679d1cc39964c2a5fc6b030b759f8ae
Eslamlinux <linuxeslam@gmail.com>
2025-12-02 20:21:27 +0200

    repeat string

--- repeatstring.cpp
+++ repeatstring.cpp
@@ -5,13 +5,14 @@
 
 using namespace std;
 
-string decodeString(const string& s) {
+std::string string_builder(std::string expression) 
+{
     stack<int> countStack;
     stack<string> resultStack;
     string currentString = ""; 
     int k = 0;                  
 
-    for (char c : s) {
+    for (char c : expression) {
         if (isdigit(c)) 
         {
             k = k * 10 + (c - '0');
@@ -46,21 +47,18 @@ string decodeString(const string& s) {
     return currentString;
 }
 
-// دالة wrapper لتلبية متطلبات التحدي (استقبال string وإرجاع string)
-std::string remove_duplicate(std::string arr) {
-    return decodeString(arr);
-}
+
+
 
 // مثال للاستخدام والاختبار
 int main() {
-    cout << "Input 1: 4a" << endl;
-    cout << "Output 1: " << remove_duplicate("4a") << endl; // Output: aaaa
+    cout  << string_builder("4a") << endl; // Output: aaaa
 
     cout << "\nInput 2: 3[a2[c]]" << endl;
-    cout << "Output 2: " << remove_duplicate("3[a2[c]]") << endl; // Output: accaccacc
+    cout << "Output 2: " << string_builder("3[a2[c]]") << endl; // Output: accaccacc
 
     cout << "\nInput 3: 2[ab]3[c]" << endl;
-    cout << "Output 3: " << remove_duplicate("2[ab]3[c]") << endl; // Output: ababccc
+    cout << "Output 3: " << string_builder("2[ab]3[c]") << endl; // Output: ababccc
     
     return 0;
 }
