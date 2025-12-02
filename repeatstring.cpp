263de3c82dbfc287f1bfb8db35de5413052659e0
Eslamlinux <linuxeslam@gmail.com>
2025-12-02 21:21:18 +0200

    repeat string

--- repeatstring.cpp
+++ repeatstring.cpp
@@ -2,59 +2,57 @@
 #include <string>
 #include <stack>
 #include <algorithm>
+#include <cctype>
 
-std::string string_builder(std::string expression) 
-{
+
+std::string string_builder(std::string expression) {
     std::stack<int> countStack;
     std::stack<std::string> resultStack;
     std::string currentString = ""; 
     int k = 0;                  
 
-    for (char c : expression) 
-    {
-        if (isdigit(c)) 
-        {
+    for (char c : expression) {
+        if (std::isdigit(c)) {
             k = k * 10 + (c - '0');
         } 
-        else if (c == '[') 
-        {
+        else if (c == '[') {
             countStack.push(k);
             resultStack.push(currentString);
             currentString = "";
             k = 0;
         } 
-        else if (c == ']') 
-        {
+        else if (c == ']') {
             int repeatCount = countStack.top();
             countStack.pop();
             std::string repeatedString = "";
-            for (int i = 0; i < repeatCount; ++i) 
-            {
-                repeatedString += currentString;
+            if (repeatCount > 0) {
+                for (int i = 0; i < repeatCount; ++i) {
+                    repeatedString += currentString;
+                }
             }
+            
             std::string prevString = resultStack.top();
             resultStack.pop();
+            
             currentString = prevString + repeatedString;
-
         } 
-        else 
-        {
+        else {
             currentString += c;
         }
     }
-    if (k > 0) 
-    {
-        std::string finalString = "";
-        for (int i = 0; i < k; ++i) 
-        {
-            finalString += currentString;
+    if (k > 0 && currentString.length() > 0 && countStack.empty()) {
+        char charToRepeat = currentString.back();
+        std::string baseString = currentString.substr(0, currentString.length() - 1);
+        
+        std::string repeatedPart = "";
+        for (int i = 0; i < k; ++i) {
+            repeatedPart += charToRepeat;
         }
-        return finalString;
+        
+        return baseString + repeatedPart;
     }
-        return currentString;
+    return currentString;
 }
-
-
 // مثال للاستخدام والاختبار
 int main() {
     std::cout << "Input 1: 4a" << std::endl;
