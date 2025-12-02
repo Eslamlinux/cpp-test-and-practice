386180ef49301fb494b9b5624441e552224b7177
Eslamlinux <linuxeslam@gmail.com>
2025-12-02 02:37:07 +0200

    left_digit

--- left_digit.cpp
+++ left_digit.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
+#include <string>
 
 int left_digit(std::string strParam) { 
+    for(auto c:strParam)
+    {
+        if(isdigit(c))
+        return std::stoi(c);
+    }
+    return 0;
 }
 
 int main()
 {
-
+std::cout << left_digit("Welcome2back") <<std::endl;
 
 }
