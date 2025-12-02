938cc0e95d1ebc9dfa968b12f216fa8badeb2ac3
Eslamlinux <linuxeslam@gmail.com>
2025-12-02 06:20:31 +0200

    remove_duplicate

--- remove_duplicate.cpp
+++ remove_duplicate.cpp
@@ -6,3 +6,10 @@ std::list<int> tolist(arr.begin(),arr.end());
 std::vector<int> result(tolist.begin(),tolist.end());
 return result;
 }
+int main()
+{
+    for(auto c : remove_duplicate({7, 8, 9, 7}))
+    {
+        std::cout << c << " ";
+    }
+}
