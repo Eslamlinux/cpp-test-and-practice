427733f9ff464c9335c58ca85a88fc91e17e5dde
Eslamlinux <linuxeslam@gmail.com>
2025-12-04 00:19:06 +0200

    can form

--- canForm.cpp
+++ canForm.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
-
+#include <algorithm>
+#include <vector>
+void to_lower_case(std::string& str)
+{
+    std::transform(str.begin(),str.end(),str.begin(),[](unsigned char c){
+    return std::tolower(c);
+    });
+}
 std::string canForm(std::string source,std::string target) { 
-    for(int i = 0; i < target.length(); i++)
-    {
-        if(!tolower(source.find(target[i])))
-        return "no";
+    to_lower_case(source);
+    to_lower_case(target);
+std::vector<int> char_counts(26, 0);
+    for (char c : source) {
+        if (c >= 'a' && c <= 'z') {
+            char_counts[c - 'a']++;
+        }
+    }
+    for (char c : target) {
+        if (c >= 'a' && c <= 'z') {
+            int index = c - 'a';
+            if (char_counts[index] > 0) 
+                char_counts[index]--;
+            else 
+                return "no";
+        }
     }
     return "yes";
 }
