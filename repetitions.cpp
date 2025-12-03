0d0e8faa26364fdeaedffe9e4e95c85a73c6c8ae
Eslamlinux <linuxeslam@gmail.com>
2025-12-03 22:05:34 +0200

    repeat string

--- repetitions.cpp
+++ repetitions.cpp
@@ -5,17 +5,27 @@ using namespace std;
 
 int repetitions(string s) { 
     size_t result = 0;
-    for(char c: s)
+    for(int i = 0; i < s.size(); i++)
     {
         size_t temp = 0;
-        temp += count(s.begin()+c, s.end(),c);
+        for(int t = i -1; t < s.size(); t++)
+        {
+            if(s[i] == s[t])
+                temp++;
+            else
+                break;
+        }
         if(temp > result)
-        result = temp;
+                result = temp;
     }
-    return result ;
+    return  result ;
 }
 
 int main()
 {
+std::cout << repetitions("AMMMDDCCCCMDM") <<std::endl;
+std::cout << repetitions("AAAAAAAAAA") <<std::endl;
+std::cout << repetitions("AAAABBBCCD") <<std::endl;
 std::cout << repetitions("AMMMDDCCCMD") <<std::endl;
+std::cout << repetitions("MZSDDDD") <<std::endl;
 }
