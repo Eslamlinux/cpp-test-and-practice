d55bca6b37197afa848b7fa93274858a334d6990
Eslamlinux <linuxeslam@gmail.com>
2025-12-04 17:05:50 +0200

    k sum sub set

--- kSumSubset.cpp
+++ kSumSubset.cpp
@@ -2,14 +2,17 @@
 #include <string>
 
 bool kSumSubset(std::string dateString) { 
-    std::string temp = dateString[0] + dateString[1] +dateString[2] + dateString[3];
+    std::string temp = dateString.substr(0,4);
     if(stoi(temp) > 2021 || stoi(temp) < 1825)
     return false;
-return true;
+    return true;
 }
 
 int main()
 {
 std::cout << kSumSubset("2099-08-02T00:00:00") <<std::endl;
+std::cout << kSumSubset("2017-10-21T00:00:00") <<std::endl;
+std::cout << kSumSubset("1111-08-29T00:00:00") <<std::endl;
+std::cout << kSumSubset("3020-01-01T00:00:00") <<std::endl;
 
 }
