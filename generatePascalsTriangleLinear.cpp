663984db6cb75d50ac4d0ec4175b0308087f5412
Eslamlinux <linuxeslam@gmail.com>
2025-12-04 13:43:25 +0200

    generate Pascals Triangle Linear

--- generatePascalsTriangleLinear.cpp
+++ generatePascalsTriangleLinear.cpp
@@ -5,8 +5,15 @@ std::vector<int>generatePascalsTriangleLinear(int num)
 {
     if(num <= 0)
     return {};
-    
+    std::vector<int>result;
+    std::vector<int>previous;
+    for(int i = 1;i <= num; i++)
+    {
+        std::vector<int>current;
+        
+    }
 
+    return result;
 }
 int main()
 {
