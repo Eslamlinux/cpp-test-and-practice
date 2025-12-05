bde34e2785bf6ae421f3ddee5530e40d44b8a441
Eslamlinux <linuxeslam@gmail.com>
2025-12-05 23:57:04 +0200

    pangram

--- DetectPangram.cpp
+++ DetectPangram.cpp
@@ -1,3 +1,10 @@
+/*
+A bookseller has lots of books classified in 26 categories labeled A, B, C, ..., Z. Each book has a code of at least 3 characters. The 1st character of a code is a capital letter which defines the book category.
+
+In the bookseller's stocklist each code is followed by a space and by a positive integer, which indicates the quantity of books of this code in stock.
+
+
+*/
 #include <iostream>
 #include <vector>
 
