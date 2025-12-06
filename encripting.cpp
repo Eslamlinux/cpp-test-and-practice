13a3652df86c5792b2d610e42c9f0d7f8ae664ae
Eslamlinux <linuxeslam@gmail.com>
2025-12-07 01:46:13 +0200

    test bill

--- encripting.cpp
+++ encripting.cpp
@@ -6,6 +6,6 @@ int main()
     char S ='S' ^3,A= 'A',U = 'U',D= 'D',I='I',R='R',B='B';
     std::cout << "\'HELLO\' \\\a\\// \"Eslam\"" << std::endl;
     std::cout << "\'HELLO\' //\\\a\\ \"Eslam\""<< std::endl;
-    wchar_t e = 'إ';
+    wchar_t e = L'55461';
     std::cout << e;
 }
