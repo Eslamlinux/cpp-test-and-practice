99ee104feb02749931242cf4b90192a3d25c8d24
Eslamlinux <linuxeslam@gmail.com>
2025-12-07 21:57:27 +0200

    StockList

--- encripting.cpp
+++ encripting.cpp
@@ -6,6 +6,10 @@ int main()
     char S ='S' ^3,A= 'A',U = 'U',D= 'D',I='I',R='R',B='B';
     std::cout << "\'HELLO\' \\\a\\// \"Eslam\"" << std::endl;
     std::cout << "\'HELLO\' //\\\a\\ \"Eslam\""<< std::endl;
-    wchar_t e = L'55461';
-    std::cout << e;
+    wchar_t e = L'إ';
+    std::wstring s = L"إسلام";
+    std::wcout << (int)e << std::endl;
+    std::wcout << s << std::endl;
+    std::wcout << sizeof(e) << std::endl;
+
 }
