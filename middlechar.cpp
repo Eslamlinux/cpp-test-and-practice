53bd7115d610a2e5bca0d19ef95065a32ca10a93
Eslamlinux <linuxeslam@gmail.com>
2025-12-08 01:47:50 +0200

    middel char

--- middlechar.cpp
+++ middlechar.cpp
@@ -1,19 +1,12 @@
 #include <iostream>
 
 std::string middle_char(std::string word) { 
-    std::string result ="";
     if(word.length() % 2 == 0)
-    {
-        result = word[(word.length() /2)-1] + word[word.length() /2];
-    }
-    else
-    {
-        result = word[word.length()/2];
-    }
-    return result;
+        return word.substr((word.length() /2)-1 , word.length() /2);
+        return word.substr(word.length()/2,1);
 }
 
 int main()
 {
-    std::cout << middle_char("test") << std::endl;
+    std::cout << middle_char("teist") << std::endl;
 }
