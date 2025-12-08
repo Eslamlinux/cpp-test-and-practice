44eb528027b81d0decc7e970d357b1f35782e88e
Eslamlinux <linuxeslam@gmail.com>
2025-12-08 03:09:27 +0200

    rotate

--- rotate.cpp
+++ rotate.cpp
@@ -10,18 +10,30 @@ k = 3
 */
 std::vector<int> rotate(std::vector<int> nums,int k) { 
     std::vector<int>result(nums);
-    for(int i = nums.size()-k; i < nums.size(); i++)
+    if(nums.size() > k)
     {
-        result.insert(result.begin(), nums[i]);
-        result.erase(result.end()-1);
+        for(int i = nums.size()-1; i >= nums.size()-k; i--)
+        {
+            result.insert(result.begin(), nums[i]);
+            result.erase(result.end()-1);
+        }
     }
+    else
+        for(int i = nums.size() - (k%nums.size()); i < nums.size(); i++ )
+        {
+            result.insert(result.begin(),nums[i]);
+            result.erase(result.end()-1);
+        }
     return result;
 }
 
 int main()
 {
 
-    for(auto c :rotate({1, 2, 3, 4, 5, 6, 7},3))
+    // for(auto c :rotate({1, 2, 3, 4, 5, 6, 7},3))
+    // for(auto c :rotate({-1, -100, 3, 99},2))
+    // for(auto c :rotate({1, 2, 3},4))
+    for(auto c :rotate({1, 2, 3, 4, 5},0))
     {
         std::cout << c << " " ;
     }
