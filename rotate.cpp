95067cc1cfa401b52c81ec454950522a511b7bd4
Eslamlinux <linuxeslam@gmail.com>
2025-12-08 02:04:23 +0200

    rotate

--- rotate.cpp
+++ rotate.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <vector>
 
+/*
+nums = [1, 2, 3, 4, 5, 6, 7]
+k = 3
+المخرجات (Outputs)
+
+[5,6,7,1,2,3,4]
+*/
 std::vector<int> rotate(std::vector<int> nums,int k) { 
 // write your code here ^_^
 }
