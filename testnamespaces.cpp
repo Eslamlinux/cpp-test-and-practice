e97f445ca6a78a698aaca5fc87bf95b47119c819
Eslamlinux <linuxeslam@gmail.com>
2025-12-08 06:23:27 +0200

    test namespaces

---
+++ testnamespaces.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+namespace One_Clan
+{
+    int func1(int a,int b)
+    {
+        praivte:
+        return a + b;
+    }
+    void printf(int a , int b)
+    {
+        std::cout << func1(a ,b) <<std::endl;
+    }
+}
+int main()
+{
+
+
+
+
+}
