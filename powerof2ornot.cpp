2de6dff4357df03cfce6e6a76a605b9deaf8420c
Eslamlinux <linuxeslam@gmail.com>
2025-12-09 14:37:29 +0200

    power of 2 or not

--- powerof2ornot.cpp
+++ powerof2ornot.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 
 std::string powerof2(int num) { 
-return num % 2 == 0? to_string(num) + " is power of 2"; 
+return num % 2 == 0? std::to_string(num) + " is power of 2" : std::to_string(num) + " is not a power of 2";
 }
 
 int main()
 {
 
 std::cout<< powerof2(8) << std::endl;
+
 }
