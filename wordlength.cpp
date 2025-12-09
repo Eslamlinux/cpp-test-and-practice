20659f9188df542059d6bd009f285a4ac3c8209d
Eslamlinux <linuxeslam@gmail.com>
2025-12-09 15:29:42 +0200

    word length

--- wordlength.cpp
+++ wordlength.cpp
@@ -3,11 +3,16 @@
 
 std::vector<int> word_length(std::vector<std::string> arr) { 
     std::vector<int>result;
+    for(std::string c:arr)
+    {
+        result.push_back(c.length());
+    }
+    return result;
 }
 
 int main()
 {
-for(int c:word_length({}))
+for(int c:word_length({"Code", "hub"}))
 {
     std::cout << c << " " ;
 }
