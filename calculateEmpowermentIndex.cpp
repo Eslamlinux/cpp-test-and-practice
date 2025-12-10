78b6c0807dbdf2e7d31442c566ab269c60b86a7f
Eslamlinux <linuxeslam@gmail.com>
2025-12-11 00:51:11 +0200

    calculate empowerment index

--- calculateEmpowermentIndex.cpp
+++ calculateEmpowermentIndex.cpp
@@ -3,11 +3,19 @@
 #include <vector>
 
 float calculateEmpowermentIndex(std::vector<float> achievements,std::vector<float> weights) { 
+    float sum = 0.0;
+    for(int i =0 ; i < achievements.size(); i++)
+    {
+        achievements[i] = achievements[i] * weights[i];
+        sum += achievements[i];
+    }
 
+    return sum / achievements.size();
 }
 
 int main()
 {
 
+    std::cout << calculateEmpowermentIndex({8, 7, 9.1},{1,1,1}) <<std::endl;
     return 0;
 }
