54588990c3fc2089fe4b7c8ddcf2600ac5c1262f
Eslamlinux <linuxeslam@gmail.com>
2025-12-10 22:00:46 +0200

    longest zero

--- longestzero.cpp
+++ longestzero.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 string longestZero(string strParam) { 
 std::string result ,temp;
-for(int i = 0 i < strParam.length(); i++)
+for(int i = 0; i < strParam.length(); i++)
 {
     if(strParam[i] == '0')
     {
@@ -17,4 +17,8 @@ for(int i = 0 i < strParam.length(); i++)
     }
 }
 return result;
+}
+int main()
+{
+    std::cout << longestZero("011100001000001100") <<std::endl;
 }
