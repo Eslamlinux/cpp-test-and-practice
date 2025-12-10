637f1814ade3854bca6bf04f998b9e7cf04b83ee
Eslamlinux <linuxeslam@gmail.com>
2025-12-10 22:40:02 +0200

    primes_nums

--- primes_nums.cpp
+++ primes_nums.cpp
@@ -2,6 +2,24 @@
 #include <vector>
 #include <cmath>
 
+
+bool is_primary(int number)
+{
+    if(number ==1)
+    {
+        return false;
+    }
+    int the_end = sqrt(number);
+    for(int i = 2; i <= the_end; i++)
+    {
+        if(number % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
 std::vector<int> primes_nums(std::vector<int> numbers) { 
-// write your code here ^_^
+
 }
