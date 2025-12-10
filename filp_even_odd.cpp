59acdfa8ee1dfc86d87ee25b76ca58c7b4834bc2
Eslamlinux <linuxeslam@gmail.com>
2025-12-10 21:21:02 +0200

    filp_even_odd

--- filp_even_odd.cpp
+++ filp_even_odd.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <vector>
+
+vector<int> filp_even_odd(vector<int> &numbers) { 
+    for(int c :numbers)
+    {
+        if(c % 2 == 0)
+        {
+            c += 1;
+        }
+        c -= 1;
+    }
+    return numbers;
+}
 
 int main()
 {
 
-    
+
 }
