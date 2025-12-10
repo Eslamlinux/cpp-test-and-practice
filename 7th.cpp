d6e71629920c21e31389b75fb6301bb41812dbf0
Eslamlinux <linuxeslam@gmail.com>
2025-12-10 18:13:53 +0200

    convert To Base7

--- 7th.cpp
+++ 7th.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 
 std::string convertToBase7(int num) { 
-std::string result;
+std::string result ;
 while(num >0)
 {
-    result += std::to_string(num % 7);
+    result = std::to_string(num % 7) + result;
     num /= 7;
 }
+
 return result;
 }
 
 int main()
 {
 std::cout << convertToBase7(100) <<std::endl;
+std::cout << convertToBase7(-7) <<std::endl;
+std::cout << convertToBase7(0) <<std::endl;
+std::cout << convertToBase7(49) <<std::endl;
 
 }
