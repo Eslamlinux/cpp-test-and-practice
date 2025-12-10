bd2bd9d3035570c48860aac988b88be71b35bc55
Eslamlinux <linuxeslam@gmail.com>
2025-12-10 20:29:26 +0200

    max Lantern Brightness

--- maxLanternBrightness.cpp
+++ maxLanternBrightness.cpp
@@ -1,8 +1,19 @@
-int maxLanternBrightness(vector<int> brightness) { 
+#include <iostream>
+#include <vector>
+
+int maxLanternBrightness(std::vector<int> brightness) { 
     int result = 0;
     for(int c: brightness)
     {
-        result += brightness;
+        result += c;
     }
     return result;
+}
+
+int main()
+{
+    std::cout << maxLanternBrightness({5,6,7}) <<std::endl;
+    std::cout << maxLanternBrightness({10, -3, 5, -2, 6}) <<std::endl;
+
+    return 0;
 }
