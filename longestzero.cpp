080f4f1451cdaacb7e2af39de40d7506928b892a
Eslamlinux <linuxeslam@gmail.com>
2025-12-10 21:59:14 +0200

    longest zero

---
+++ longestzero.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+string longestZero(string strParam) { 
+std::string result ,temp;
+for(int i = 0 i < strParam.length(); i++)
+{
+    if(strParam[i] == '0')
+    {
+        temp += '0';
+    }
+    else
+    {
+        if(temp.length() > result.length())
+        result =temp;
+        temp = "";
+    }
+}
+return result;
+}
