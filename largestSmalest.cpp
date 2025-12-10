a283e5a2ced057fd05b789cf144c1e23a4cdd446
Eslamlinux <linuxeslam@gmail.com>
2025-12-10 21:45:10 +0200

    largest and smallest

---
+++ largestSmalest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+std::vector<int> largest_smallest(std::vector<int> array_values) {  
+std::sort(array_values.begin(),array_values.end(),std::grater<int>());
+int largest = array_values[0];
+std::sort(array_values.begin(),array_values.end());
+int smallest = array_values[0]; 
+return {largest,smallest};
+}
+
