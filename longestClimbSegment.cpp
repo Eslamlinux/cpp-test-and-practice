97735db703b214dc4b1c63ea457c978df07742c2
Eslamlinux <linuxeslam@gmail.com>
2025-12-11 21:05:17 +0200

    longestClimbSegment

--- longestClimbSegment.cpp
+++ longestClimbSegment.cpp
@@ -1,12 +1,17 @@
 //longestClimbSegment
 #include <iostream>
-using namespace std;
+#include <vector>
 
-int longestClimbSegment(vector<int> heights) { 
-// write your code here ^_^
+int longestClimbSegment(std::vector<int> heights) { 
+int result =0;
+for(int i =0; i < heights.size(); i++)
+{
+    
+}
+return result ;
 }
 
 int main()
 {
-    
+std::cout << longestClimbSegment({1, 2, 3, 2, 3, 4, 1}) <<std::endl;
 }
