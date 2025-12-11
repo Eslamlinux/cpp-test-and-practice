cab8668aa6e959d8307e6e79225cfbd9119e3db6
Eslamlinux <linuxeslam@gmail.com>
2025-12-11 02:08:55 +0200

    oct_to_dec

--- oct_to_dec.cpp
+++ oct_to_dec.cpp
@@ -1,7 +1,13 @@
+//oct_to_dec
 #include <iostream>
+
+int oct_to_dec(int octal_number) { 
+return (int) octal_number;
+}
+
 
 int main()
 {
-
+    std::cout << oct_to_dec(5252) <<std::endl; // 2730
     return 0;
 }
