4345f00b3e956f126642e5bef9e015c53cd3068c
Eslamlinux <linuxeslam@gmail.com>
2025-12-12 00:51:30 +0200

    octal

--- oct_to_dec.cpp
+++ oct_to_dec.cpp
@@ -20,10 +20,12 @@ int oct_to_dec(int octal_number) {
     return res;
 }
 
-
 int main()
 {
     std::cout << oct_to_dec(4444) <<std::endl; // 5252 2730  /4444
     std::cout << oct_to_dec(8888) <<std::endl; // 5252 2730  /4444
     return 0;
 }
+
+
+
