5bba33935b0bf21a684369573cf04e963edfc332
Eslamlinux <linuxeslam@gmail.com>
2025-12-12 00:44:51 +0200

    octal

--- oct_to_dec.cpp
+++ oct_to_dec.cpp
@@ -3,6 +3,7 @@
 #include <cmath>
 #include <vector>
 int oct_to_dec(int octal_number) { 
+
     std::string numtostr = std::to_string(octal_number);
     std::vector<int> power;
     int res =0;
@@ -10,6 +11,7 @@ int oct_to_dec(int octal_number) {
     {
         power.insert(power.begin(),pow(8,i));
     }
+
     for(int i = 0 ;i < power.size();i++)
     {
         res += power[i] * (numtostr[i] - '0');
@@ -22,5 +24,6 @@ int oct_to_dec(int octal_number) {
 int main()
 {
     std::cout << oct_to_dec(4444) <<std::endl; // 5252 2730  /4444
+    std::cout << oct_to_dec(8888) <<std::endl; // 5252 2730  /4444
     return 0;
 }
