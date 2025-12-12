3442f467f7d43fe2a1506324e84582acc830fb5f
Eslamlinux <linuxeslam@gmail.com>
2025-12-12 21:43:40 +0200

    compare_two_words

--- compare_two_words.cpp
+++ compare_two_words.cpp
@@ -1,9 +1,12 @@
 //compare_two_words
 
 #include <iostream>
+bool compare_two_words(std::string s1,std::string s2) { 
+return s1[s1.size()-1] == s2[s2.size() -1];
+}
 
 int main()
 {
+std::cout << compare_two_words("abc","abc") <<std::endl;
 
-    
 }
