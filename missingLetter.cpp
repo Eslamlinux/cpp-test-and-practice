bf3fcc4add6d65b4377cb772a681a7f7c6e930d9
Eslamlinux <linuxeslam@gmail.com>
2025-12-12 21:31:27 +0200

    missing letter

--- missingLetter.cpp
+++ missingLetter.cpp
@@ -1,9 +1,18 @@
 //missingLetter
 
 #include <iostream>
-//No Missing Letter
-std::string missingLetter(std::string txt) { 
 
+std::string missingLetter(std::string txt) { 
+    std::string result = "No Missing Letter";
+    for(int i = 0 ; i < txt.length() -1; i++)
+    {
+        if(txt[i] +1 != txt[i+1])
+        {
+            result = txt[i] + 1;
+            break;
+        }
+    }
+    return result;
     
 }
 
