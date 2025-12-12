e994ca166bc5182de729f932145c486620104eab
Eslamlinux <linuxeslam@gmail.com>
2025-12-12 21:53:14 +0200

    most_frequent_element

--- most_frequent_element.cpp
+++ most_frequent_element.cpp
@@ -4,5 +4,9 @@
 #include <vector>
 
 int most_frequent_element(std::vector<int> arr) { 
-// write your code here ^_^
+
+}
+int main()
+{
+    std::cout << most_frequent_element({13, 2, 1, 2, 10, 2, 1, 1, 2, 2}) <<std::endl;
 }
