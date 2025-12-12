987c720c6ac8598cea631ad59f8f8e7b3b2ff7d8
Eslamlinux <linuxeslam@gmail.com>
2025-12-12 21:11:38 +0200

    missing Letter

--- find_prefix.cpp
+++ find_prefix.cpp
@@ -5,15 +5,18 @@
 std::vector<std::string> find_prefix(std::vector<std::string> words,std::string text) 
 	{
 	std::vector<std::string> result;
-	std::tolower(text);
+	std::vector<std::string> temp(words);
+	std::transform(text.begin(),text.end(),text.begin(),::tolower);
 	for(int i = 0; i < words.size();i++)
 	{
-std::transform(words[i].begin(),words[i].end(),words[i].begin(),::tolower);
-	
-	size_t pos =words[i].find(text);
+  	std::transform(temp[i].begin(),temp[i].end(),temp[i].begin(),::tolower);
+	size_t pos = temp[i].find(text);
 	if(pos != std::string::npos)
 	result.push_back(words[i]);
 	}
+	if(result.empty())
+		result.push_back("No matches found");
+
 	return result;
 	}
 int main()
