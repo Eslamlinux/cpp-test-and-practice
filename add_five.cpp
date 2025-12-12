86508e32b58f1fd176a620c8eccfa3b15129689a
Eslamlinux <linuxeslam@gmail.com>
2025-12-13 00:13:46 +0200

    add_five

--- add_five.cpp
+++ add_five.cpp
@@ -1,2 +1,11 @@
 //add_five to array
 
+#include <iostream>
+#include <vector>
+std::vector<std::string> add_five(std::vector<std::string> arr) { 
+for(auto& c:arr)
+{
+    c += "5";
+}
+return arr;
+}
