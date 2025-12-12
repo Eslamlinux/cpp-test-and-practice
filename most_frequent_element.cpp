cf451333dbfa7333dcf04a5c651d5b9ad1804f1a
Eslamlinux <linuxeslam@gmail.com>
2025-12-12 22:31:26 +0200

    most_frequent_element

--- most_frequent_element.cpp
+++ most_frequent_element.cpp
@@ -23,4 +23,6 @@ int main()
 {
     std::cout << most_frequent_element({13, 2, 1, 2, 10, 2, 1, 1, 2, 2}) <<std::endl; //2
     std::cout << most_frequent_element({1, 2, 3, 4, 1, 5, -5, -5, -5, -5, -5, 6}) <<std::endl; //-5
+    std::cout << most_frequent_element({1, 2, 3, 3, 3, 3}) <<std::endl; //3
+    std::cout << most_frequent_element({0, 5, 6, 2, 1, 5}) <<std::endl; //3
 }
