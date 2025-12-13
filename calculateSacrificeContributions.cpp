a90dfeb7a8f2dffe7b9990afa8f86d0d522d1520
Eslamlinux <linuxeslam@gmail.com>
2025-12-13 20:04:56 +0200

    calculate Sacrifice Contributions

--- calculateSacrificeContributions.cpp
+++ calculateSacrificeContributions.cpp
@@ -1,1 +1,8 @@
 //calculate Sacrifice Contributions
+
+#include <iostream>
+#include <vector>
+
+std::vector<float> calculateSacrificeContributions(std::vector<std::string> names,std::vector<float> shares,float totalCost) { 
+// write your code here ^_^
+}
