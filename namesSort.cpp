0ae14350344efad0d95d7c4efce18bb34825ff97
Eslamlinux <linuxeslam@gmail.com>
2025-12-13 22:26:30 +0200

    names Sort

--- namesSort.cpp
+++ namesSort.cpp
@@ -3,8 +3,11 @@
 #include <iostream>
 #include <vector>
 
-std::vector<std::string> namesSort(std::vector<std::string> namesArray,std::string order) { 
-
+std::vector<std::string> namesSort(std::vector<std::string> namesArray,std::string order) 
+{ 
+std::sort(namesArray.begin(),namesArray.end(),[](const std::string a,const std::string b){
+    return order == "DESC" ? a.length() > b.length() : a.length() < b.length() ::length;
+})
 }
 int main()
 {
