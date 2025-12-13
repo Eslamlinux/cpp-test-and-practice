c66a686ed7da42a7fa9f392a216919d966d9a725
Eslamlinux <linuxeslam@gmail.com>
2025-12-13 23:54:50 +0200

    time period

--- timePeriod.cpp
+++ timePeriod.cpp
@@ -12,14 +12,18 @@ std::tm tm2{};
 const char* format = "%d-%d-%d*%c%d:%d:%d";
 int success1 = std::sscanf(date1.c_str(),format,&tm1.tm_year, &tm1.tm_mon, &tm1.tm_mday, &tm1.tm_hour ,&tm1.tm_min, &tm1.tm_sec);
 int success2 = std::sscanf(date2.c_str(),format,&tm2.tm_year, &tm2.tm_mon, &tm2.tm_mday, &tm2.tm_hour, &tm2.tm_min, &tm2.tm_sec);
+
+if(success1 != 6 || success2 != 6)
+return false;
+
 tm1.tm_year -= 1900;
 tm1.tm_mon -= 1;
 tm2.tm_year -= 1900;
 tm2.tm_mon -= 1;
 
-if(success1 != 6 || success2 != 6)
-return false;
-
+std::time_t t1 = std::mktime(&tm1);
+std::time_t t2 = std::mktime(&tm2);
+if(t1 == (std::time_t) -1 || t2 == (std::time_t) -1)
 return 1;
 
 }
