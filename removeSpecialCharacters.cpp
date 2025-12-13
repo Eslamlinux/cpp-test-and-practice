c8ebebee31e1874f3e49dee600acfee35fd639b6
Eslamlinux <linuxeslam@gmail.com>
2025-12-14 01:29:52 +0200

    remove Special Characters

--- removeSpecialCharacters.cpp
+++ removeSpecialCharacters.cpp
@@ -6,7 +6,7 @@ std::string removeSpecialCharacters(std::string strParam)
     std::string result;
     for(char c: strParam)
     {
-        if(isalpha(c) ||c == '_'||c == '.')
+        if(isalpha(c) ||c == '_'||c == '-'|| c == ' ')
         result += c;
     }
     return result;
@@ -14,4 +14,7 @@ std::string removeSpecialCharacters(std::string strParam)
 int main()
 {
     std::cout << removeSpecialCharacters("Are_u_coming?") << std::endl;
+    std::cout << removeSpecialCharacters("Hi How are you!") << std::endl;
+    std::cout << removeSpecialCharacters("Solve-the-challenge") << std::endl;
+    std::cout << removeSpecialCharacters("Nora Rahaf$") << std::endl;
 }
