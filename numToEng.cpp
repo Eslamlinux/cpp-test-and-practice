be9046c64c24190131221213c9dd5a7947e5b179
Eslamlinux <linuxeslam@gmail.com>
2025-12-13 20:33:09 +0200

    num To Eng

--- numToEng.cpp
+++ numToEng.cpp
@@ -2,7 +2,30 @@
 #include <iostream>
 
 std::string numToEng(int n) { 
-
+    std::string num = std::to_string(n);
+    std::string result;
+    for(char c:num)
+    {
+        switch(c -'0')
+        {
+            case 0:
+            result += '0';
+            break;
+            case 1:
+            result += '1';
+            break;
+            case 2:
+            result += '2';
+            break;
+            case 3:
+            result += '3';
+            break;
+            case 4:
+            result += '4';
+            break;
+            
+        }
+    }
 }
 
 int main()
