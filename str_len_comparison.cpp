20ff0840aa30354bdfd6f8251581629813218e24
Eslamlinux <linuxeslam@gmail.com>
2025-12-14 01:02:10 +0200

    str len comparison

--- str_len_comparison.cpp
+++ str_len_comparison.cpp
@@ -2,11 +2,13 @@
 #include <iostream>
 #include <vector>
 
-bool str_len_comparison(std::vector<std::string> words) { 
-    
+bool str_len_comparison(std::vector<std::string> words) 
+{ 
+
 }
 
 int main()
 {
-
+    std::cout << str_len_comparison({"Satr", "CoderHub"}) <<std::endl;
+    std::cout << str_len_comparison({"a", "b"}) <<std::endl;
 }
