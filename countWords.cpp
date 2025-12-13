62d6880093cad52d40b894392e048386cd8bf2e2
Eslamlinux <linuxeslam@gmail.com>
2025-12-13 21:29:32 +0200

    count Words

--- countWords.cpp
+++ countWords.cpp
@@ -1,1 +1,18 @@
 //count Words
+#include <iostream>
+
+
+int countWords(std::string txt) { 
+    std::stringstream ss(txt);
+    int counter = 0;
+    for(std::string c:ss)
+    {
+        
+    }
+    
+}
+int main()
+{
+    std::cout << countWords("Plant hope in the hearts of others") <<std::endl;
+
+}
