87400891335793968cb5b3ab72bfe311d00ea62a
Eslamlinux <linuxeslam@gmail.com>
2025-12-14 01:25:32 +0200

    remove Special Characters

--- removeSpecialCharacters.cpp
+++ removeSpecialCharacters.cpp
@@ -1,12 +1,17 @@
 //remove Special Characters
 #include <iostream>
 
-
 std::string removeSpecialCharacters(std::string strParam) 
 { 
-
+    std::string result;
+    for(char c: strParam)
+    {
+        if(isalpha(c) ||c == '_'||c == '.')
+        result += c;
+    }
+    return result;
 }
 int main()
 {
-    std::cout << removeSpecialCharacters("") << std::endl;
+    std::cout << removeSpecialCharacters("Are_u_coming?") << std::endl;
 }
