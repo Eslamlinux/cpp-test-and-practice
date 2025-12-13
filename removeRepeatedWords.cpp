c36d3205cab9f77aadb33e664eadd3f218a9d289
Eslamlinux <linuxeslam@gmail.com>
2025-12-13 17:28:09 +0200

    remove Repeated Words

--- removeRepeatedWords.cpp
+++ removeRepeatedWords.cpp
@@ -1,9 +1,42 @@
 //remove Repeated Words
 #include <vector>
+#include <string>
 #include <iostream>
 
 std::string removeRepeatedWords(std::string sentence) { 
+    std::vector<std::string> words;
+    std::string temp, result;
+    for(int i =0; i < sentence.length(); i++)
+    {
+        if(sentence[i] != ' ')
+        {
+            temp += sentence[i];
+        }
+        else
+        {
+            for(int t =0; t < words.size();t++)
+            {
+                if(temp == words[t])
+                {
+                    temp = "";
+                    continue;
+                }
+                else
+                {
+                    words.push_back(temp);
+                    temp == "";
+                }
+            }
+        }
+    }
+    // if(!temp.empty())
 
+    for(auto c : words)
+    {
+        result += c + " ";
+    }
+    result.pop_back();
+    return result;
 }
 int main()
 {
