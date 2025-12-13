9a20b171a9da2bfe29a65c33959206252590069f
Eslamlinux <linuxeslam@gmail.com>
2025-12-13 20:07:37 +0200

    calculate Sacrifice Contributions

--- calculateSacrificeContributions.cpp
+++ calculateSacrificeContributions.cpp
@@ -3,6 +3,15 @@
 #include <iostream>
 #include <vector>
 
-std::vector<float> calculateSacrificeContributions(std::vector<std::string> names,std::vector<float> shares,float totalCost) { 
-// write your code here ^_^
+std::vector<float> calculateSacrificeContributions(std::vector<std::string> names,std::vector<float> shares,float totalCost) 
+{ 
+
+}
+
+int main()
+{
+    for(auto c : calculateSacrificeContributions({"أحمد", "سارة"},{0.5, 0.5},200))
+    {
+        std::cout << c << " ";
+    }
 }
