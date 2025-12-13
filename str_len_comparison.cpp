8ac8ce7839a8e6a40d64cc8a6026514d8b4ddb8b
Eslamlinux <linuxeslam@gmail.com>
2025-12-14 00:59:50 +0200

    str len comparison

--- str_len_comparison.cpp
+++ str_len_comparison.cpp
@@ -1,12 +1,11 @@
 //str len comparison
 #include <iostream>
-using namespace std;
 
-bool str_len_comparison(vector<string> words) { 
+bool str_len_comparison(std::vector<std::string> words) { 
 // write your code here ^_^
 }
 
 int main()
 {
-    
+
 }
