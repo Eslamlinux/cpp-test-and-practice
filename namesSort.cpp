4a7dcc062d57e714e6c48ec675e8d6babe670ebd
Eslamlinux <linuxeslam@gmail.com>
2025-12-13 22:33:07 +0200

    name sort

--- namesSort.cpp
+++ namesSort.cpp
@@ -7,8 +7,7 @@
 std::vector<std::string> namesSort(std::vector<std::string> namesArray,std::string order) 
 { 
 std::sort(namesArray.begin(),namesArray.end(),[order](const std::string a,const std::string b){
-    return order == "DESC" ? a.length() > b.length() : a.length() < b.length() ;
-});
+    return order == "DESC" ? a.length() > b.length() : a.length() < b.length() ; });
 return namesArray;
 }
 int main()
