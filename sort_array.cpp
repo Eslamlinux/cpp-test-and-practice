6ab7f69bb7b6ffe99f46190ce766d53e24014ad8
Eslamlinux <linuxeslam@gmail.com>
2025-12-14 19:13:03 +0200

    sort array

--- sort_array.cpp
+++ sort_array.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+std::vector<int> sort_array(std::vector<int> elements_array)
+{ 
+    std::sort(elements_array.begin(),elements_array.end());
+return elements_array;
+}
 
 int main()
 {
