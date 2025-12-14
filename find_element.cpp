564e24a1b04e8cb8e0c0483cdd03d419b975f5a0
Eslamlinux <linuxeslam@gmail.com>
2025-12-14 20:24:19 +0200

    find_element

--- find_element.cpp
+++ find_element.cpp
@@ -2,7 +2,10 @@
 #include <vector>
 bool find_element(vector<int> elements_array,int element) 
 {
-
+    std::vector<int> tofind = std::find(elements_array.begin(),elements_array.end(),element);
+    if(tofind == element_array.end())
+       return false;
+   return true; 
 }
 int main()
 {
