a28a3c5eec185f25f9756a7987270d32f4aa5789
Eslamlinux <linuxeslam@gmail.com>
2025-12-14 19:00:25 +0200

    sub arrays

--- sub_arrays.cpp
+++ sub_arrays.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 #include <vector>
 std::vector<int> sub_arrays(std::vector<int> arr1,std::vector<int> arr2) { 
-
+for(int i = 0; i < arr2.size(); i++)
+{
+    arr2[i] = arr2[i] - arr1[i];
+}
+return arr2;
 }
 int main()
 {
