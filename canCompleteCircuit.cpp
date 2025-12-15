fd47f554e1a3fb026e8e1be325b251bdc1ef9564
Eslamlinux <linuxeslam@gmail.com>
2025-12-15 20:36:12 +0200

    canCompleteCircuit

--- canCompleteCircuit.cpp
+++ canCompleteCircuit.cpp
@@ -1,22 +1,35 @@
 #include <iostream>
 #include <vector>
 
-int canCompleteCircuit(std::vector<int> gas,std::vector<int> cost)
+int ca(std::vector<int> gas,std::vector<int> cost)
 {
    int startindex = 0;
-   int fuelmeter = gas[i];
+   int fuelmeter = 0;
     for(int i = 0; i < gas.size();i++)
     {
-    fuelmeter += gas[i] - cost[i];
-   if(fuelmeter > 0)
-    startindex = i;
+        fuelmeter += gas[i] - cost[i];
+        if(fuelmeter > 0)
+        {
+        startindex = i;
+        }
     }
     return startindex;
 }
 int main()
 {
-    for(auto c : canCompleteCircuit({1,2,3,4,5},{3,4,5,1,2}))
+    std::vector<int>gg = {1,2,3,4,5};
+    std::vector<int>co = {3,4,5,1,2};
+   //for(int a: ca(gg, co))
+    //{
+      //  std::cout << a << " ";
+    
+    //}
+    //
+
+    std::vector<int> toprnt(ca(gg, co));
+    for(int i = 0 ; i < toprnt.size(); i++)
     {
-        std::cout << c << " ";
+        std::cout << toprnt[i];
     }
+return 0;
 }
