1e63088c3929ca72285fcdf4a5ed6320767c6b21
Eslamlinux <linuxeslam@gmail.com>
2025-12-15 17:39:34 +0200

    sort array by a and b

--- sort_array_fromAtoB.cpp
+++ sort_array_fromAtoB.cpp
@@ -6,4 +6,12 @@ std::vector<int> sort_array(std::vector<int> my_array,std::string typeParam)
 { 
 
 }
+int main()
+{
+for(auto c: sort_array({63,12,43,56,12}, "B"))
+        {
+    std::cout << c << " ";
+        }
 
+return 0;
+}
