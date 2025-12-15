52e95a2580f6fd87b7ae5c6bafb16998db2f4569
Eslamlinux <linuxeslam@gmail.com>
2025-12-15 18:53:38 +0200

    canCompleteCircuit

--- canCompleteCircuit.cpp
+++ canCompleteCircuit.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 
-int canCompleteCircuit(std::vector<int> gas,std::vector<int> cost) { 
+int canCompleteCircuit(std::vector<int> gas,std::vector<int> cost)
+{ 
 return gas[0] != 1? -1 : gas[gas.size() /2];
 }
 int main()
