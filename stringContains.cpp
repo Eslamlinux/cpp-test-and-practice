3164a414914766648df5977d43c2d19ccadacb90
Eslamlinux <linuxeslam@gmail.com>
2025-12-15 17:09:42 +0200

    string contains

--- stringContains.cpp
+++ stringContains.cpp
@@ -3,7 +3,12 @@
 #include <iostream>
 #include <algorithm>
 
-bool stringContains(std::string firstName,std::string contains) {
-
+bool stringContains(std::string firstName,std::string contains)
+{
+size_t inor = std::find(firstName.begin(),firstName.end(),contains);
+return inor != std::npos;
 }
-
+int main()
+{
+    std::cout << stringContains("Ahmed","A") <<std::endl;
+}
