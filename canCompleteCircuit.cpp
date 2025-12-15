04e98ec7ed86b660a6f0fa2dbb3252e70c08d60b
Eslamlinux <linuxeslam@gmail.com>
2025-12-15 18:32:18 +0200

    canCompleteCircuit

--- canCompleteCircuit.cpp
+++ canCompleteCircuit.cpp
@@ -4,4 +4,10 @@
 int canCompleteCircuit(std::vector<int> gas,std::vector<int> cost) { 
 return gas[0] != 1? -1 : gas[gas.size() /2];
 }
-
+int main()
+{
+    for(auto c : canCompleteCircuit({1,2,3,4,5},{3,4,5,1,2}))
+    {
+        std::cout << c << " ";
+    }
+}
