2a5a77b885eaf4a18fcffe095879f33dad36f789
Eslamlinux <linuxeslam@gmail.com>
2025-12-17 00:00:02 +0200

    try using for alias

--- testusing.cpp
+++ testusing.cpp
@@ -5,13 +5,13 @@
 int main()
 {
 using num = int;
-
+using str =std::string;
 
 num a = 5;
 num b {10};
-
+str s = " eslam ";
 std::cout << a + b <<std::endl;
-
+std::cout << s << std::endl;
 
     return 0;
 }
