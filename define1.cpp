ca254a22b1c2a113d77e6e57d6806c3aa1fa00d4
Eslamlinux <linuxeslam@gmail.com>
2025-12-16 19:58:01 +0200

    define 1 test

---
+++ define1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#define c cout
+#define num 10
+#define num2 20
+#define num3 2.5
+
+int main()
+{
+std::c << "hello\n";
+std::c << num +num2 << std::endl;
+std::c << num +num3 << std::endl;
+
+}
