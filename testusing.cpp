342452a82778fe8009731aff0ccabd57d7557368
Eslamlinux <linuxeslam@gmail.com>
2025-12-17 00:04:49 +0200

    test using alias

--- testusing.cpp
+++ testusing.cpp
@@ -8,14 +8,19 @@ using c = char;
 using fl = float ;
 using dou = double;
 
-
+c cc ()
+{
+    std::cout << "from cc\n";
+}
 int main()
 {
 num a = 5;
 num b {10};
 str s = " eslam ";
+
 std::cout << a + b <<std::endl;
 std::cout << s << std::endl;
+cc();
 
     return 0;
 }
