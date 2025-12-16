b6ddb7bb6684a295d0ab9d74410ef672802d28f6
Eslamlinux <linuxeslam@gmail.com>
2025-12-16 20:10:08 +0200

    define 1 test

--- define1.cpp
+++ define1.cpp
@@ -4,11 +4,21 @@
 #define num 10
 #define num2 20
 #define num3 2.5
-
+#define st "string"
+#define st2 "string 2 " << "string 3"
+#define s std::
+#define e endl 
+#define koma ;
+#define oneorder std::cout << "one orderd \n";
 int main()
 {
 std::c << "hello\n";
 std::c << num +num2 << std::endl;
 std::c << num +num3 << std::endl;
+s c << st <<s endl;
+
+s c << st2 << s e koma
+oneorder
+
 
 }
