5324ce4343cc2515fef2d895245a83dcb6477aad
Eslamlinux <linuxeslam@gmail.com>
2025-12-17 05:11:02 +0200

    enum test 1

--- enum1.cpp
+++ enum1.cpp
@@ -13,6 +13,7 @@ enum items
 void choseItem(bool& wo)
 {
     int chosenum;
+    std::cout << "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n";
     std::cout << "apple [1] - phone [2] - pc [3] - Exit [4]\n";
     std::cout << "chose item: ";
     std::cin >> chosenum;
@@ -20,22 +21,30 @@ void choseItem(bool& wo)
     {
         case Apple:
         system("clear");
-        std::cout << "you chose apple\n";
+        std::cout<< "         \t\t *-*-*-*\n";
+        std::cout<< "\tyou chose\t *apple*\n";
+        std::cout<< "         \t\t *-*-*-*\n";        
         break;
         case phone:
         system("clear");
-        std::cout<< "you chose phone\n";
+        std::cout<< "         \t\t *-*-*-*\n";
+        std::cout<< "\tyou chose\t *phone*\n";
+        std::cout<< "         \t\t *-*-*-*\n";
         break;
         case pc:
         system("clear");
-        std::cout<< "you chose pc\n";
+        std::cout<< "         \t\t *-*-*-*\n";
+        std::cout<< "\tyou chose\t *  pc *\n";
+        std::cout<< "         \t\t *-*-*-*\n";        
         break;
         case Exit:
         wo = false;
         break;
         default:
         system("clear");
-        std::cout<< "wrong chose \n";
+        std::cout<< "         \t\t *-*-*-*-*-*-*\n";
+        std::cout<< "\tyou chose\t *wrong chose*\n";
+        std::cout<< "         \t\t *-*-*-*-*-*-*\n";    
         break;
     }
 }
