49e765c99f392e6de8263e409716d1235bd85a0a
Eslamlinux <linuxeslam@gmail.com>
2025-12-17 21:09:39 +0200

    calculate Donation Percentages

--- calculateDonationPercentages.cpp
+++ calculateDonationPercentages.cpp
@@ -1,1 +1,7 @@
 // calculate Donation Percentages
+#include <iostream>
+
+
+vector<string> calculateDonationPercentages(vector<string> names,vector<float> donations) { 
+// write your code here ^_^
+}
