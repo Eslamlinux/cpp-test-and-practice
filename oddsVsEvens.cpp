236ce3a055a3deb79b74566749b9ab46a58ef8db
Eslamlinux <linuxeslam@gmail.com>
2025-12-17 20:37:32 +0200

    try using for alias

--- oddsVsEvens.cpp
+++ oddsVsEvens.cpp
@@ -5,11 +5,11 @@
 std::string oddsVsEvens(int num) 
 { 
     int odd = 0 ,even = 0;
-    for(auto c : std::to_string(num))
+    for(char c : std::to_string(num))
     {
-        if(c % 2 == 0)
-        even += c - '0';
-        odd += c - '0';
+        if((c -'0') % 2 == 0)
+        even += c -'0';
+        odd += c -'0';
     }
     return odd > even? "odd" : even > odd? "even" : "equal";
 }
