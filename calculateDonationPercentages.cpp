81bef1542b39e401f4350c95ff83324e3b22007b
Eslamlinux <linuxeslam@gmail.com>
2025-12-17 21:21:19 +0200

    try using for alias

--- calculateDonationPercentages.cpp
+++ calculateDonationPercentages.cpp
@@ -3,12 +3,22 @@
 #include <vector>
 
 std::vector<std::string> calculateDonationPercentages(std::vector<std::string> names,std::vector<float> donations) 
-{ 
-
+{
+    std::vector<std::string> result;
+    for(int i = 0; i < names.size(); i++)
+    {
+        result.push_back(names[i]+ ": " +  std::to_string((donations[i] * 100.0) / 100.0));
+    }
+    return result;
 }
 
 int main()
 {
+    std::vector<std::string> na {"فاطمة", "علي", "خالد"};
+    std::vector<float> fl {50.0, 25.0, 25.0};
+    for(auto c : calculateDonationPercentages(na,fl))
+    {
+        std::cout << c<< " ";
+    }
 
-    
 }
