7db833c0a1775044f8377a0fd8e8a8242a2f6d65
Eslamlinux <linuxeslam@gmail.com>
2025-12-17 21:36:07 +0200

    calculate Donation Percentages

--- calculateDonationPercentages.cpp
+++ calculateDonationPercentages.cpp
@@ -5,17 +5,28 @@
 std::vector<std::string> calculateDonationPercentages(std::vector<std::string> names,std::vector<float> donations) 
 {
     std::vector<std::string> result;
+    float sumall = 0.0;
+    for(float f :donations)
+    {
+        sumall += f;
+    }
     for(int i = 0; i < names.size(); i++)
     {
-        result.push_back(names[i]+ ": " +  std::to_string((donations[i] / 100.0) * 100.0) + "%");
+        int temp =(donations[i] / 100) * 100;
+        result.push_back(names[i]+ ": " +  std::to_string(temp)+ "%");
     }
     return result;
 }
 
 int main()
 {
-    std::vector<std::string> na {"فاطمة", "علي", "خالد"};
-    std::vector<float> fl {50.0, 25.0, 25.0};
+    // 'فاطمة: 50%, علي: 50%
+    // std::vector<std::string> na {"فاطمة", "علي", "خالد"};
+    // std::vector<float> fl {50.0, 25.0, 25.0};
+
+        std::vector<std::string> na {"فاطمة", "علي"};
+    std::vector<float> fl {100, 100};
+
     for(auto c : calculateDonationPercentages(na,fl))
     {
         std::cout << c<< " ";
