885ba859f0edf0f91650643fee0718a072bb22f1
Eslamlinux <linuxeslam@gmail.com>
2025-12-17 18:11:57 +0200

    is Numb

---
+++ isNumber.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isNumber(string s) { 
+    int cd =0 , ce = 0, cE = 0,c_ = 0, cp = 0;
+    for (char c : s) {
+                if(c == '+' && cp < 1) 
+                {
+                cp++;
+                continue ;
+                }
+                else if(c == '.' && cd < 1)
+                {
+                    cd++;
+                continue;
+                } 
+                else if(c == 'e' && ce < 1)
+                {
+                ce++;
+                continue ;
+                }
+                else if(c == 'E' && cE < 1)
+                {
+                cE++;
+                continue ;
+                }
+                else if(c == '-' & c_ < 1) 
+                {
+                c_++;
+                continue ;
+                }
+       return !isdigit(c)? false : true;
+    }   
+return true;
+}
