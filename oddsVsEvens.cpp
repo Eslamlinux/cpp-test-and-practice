09e581b415e60d7f5878d24e44e0689862365b78
Eslamlinux <linuxeslam@gmail.com>
2025-12-17 20:28:01 +0200

    odds Vs Evens & equal

--- oddsVsEvens.cpp
+++ oddsVsEvens.cpp
@@ -4,7 +4,14 @@
 
 std::string oddsVsEvens(int num) 
 { 
-
+    int odd = 0 ,even = 0;
+    for(auto c : std::to_string(num))
+    {
+        if(c % 2 == 0)
+        even += c - '0';
+        odd += c - '0';
+    }
+    
 }
 
 int main()
