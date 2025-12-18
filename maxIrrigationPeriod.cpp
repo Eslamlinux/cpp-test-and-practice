07e0c0eda5b4f38285cee80f609bf12a9d108599
Eslamlinux <linuxeslam@gmail.com>
2025-12-19 00:09:23 +0200

    trailing Zeroes

--- maxIrrigationPeriod.cpp
+++ maxIrrigationPeriod.cpp
@@ -7,11 +7,18 @@ int maxIrrigationPeriod(std::vector<int> irrigation,int water)
 { 
     if(irrigation.empty() || water < 1)
     return 0;
-
+    int Sum_Irri = 0;
+    int i =0;
+    while(Sum_Irri < water)
+    {
+        Sum_Irri += irrigation[i++];
+    }
+    return i;
 }
 
 int main()
 {
+    std::cout << maxIrrigationPeriod({1, 1, 1, 1},3) << std::endl;
     std::cout << maxIrrigationPeriod({1, 1, 1, 1},3) << std::endl;
 
     return 0;
