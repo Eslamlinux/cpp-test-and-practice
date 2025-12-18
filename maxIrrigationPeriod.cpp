dab56b0d8f6a46b717c69ab92e2881ff0d89b10b
Eslamlinux <linuxeslam@gmail.com>
2025-12-19 00:02:47 +0200

    max Irrigation Period

--- maxIrrigationPeriod.cpp
+++ maxIrrigationPeriod.cpp
@@ -5,12 +5,14 @@
 
 int maxIrrigationPeriod(std::vector<int> irrigation,int water) 
 { 
+    if(irrigation.empty() || water < 1)
+    return 0;
 
 }
 
 int main()
 {
-    std::cout << maxIrrigationPeriod({}) << std::endl;
+    std::cout << maxIrrigationPeriod({1, 1, 1, 1},3) << std::endl;
 
     return 0;
 }
