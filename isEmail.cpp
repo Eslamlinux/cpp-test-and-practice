055c1ff3fc4e798991dfcd263258cf643a63b9b2
Eslamlinux <linuxeslam@gmail.com>
2025-12-19 17:01:16 +0200

    is email

--- isEmail.cpp
+++ isEmail.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <string>
+
+bool isEmail(std::string email) 
+{ 
+
+}
+
 
 int main()
 {
-
+    std::cout << isEmail("example@example.com") << std::endl;
 
     return 0;
 }
