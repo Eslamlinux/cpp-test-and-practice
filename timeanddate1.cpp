0372ff57d44dacd26472b4708e9ef93dde3708b6
Eslamlinux <linuxeslam@gmail.com>
2025-12-20 12:37:47 +0200

    test time and date in ctim lib

--- timeanddate1.cpp
+++ timeanddate1.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <ctime>
 #include <tuple>
-
+#include <unistd.h>
 std::tuple<float,char,float,char,float> inTime()
 {
     time_t t = time(nullptr);
@@ -13,7 +13,27 @@ std::tuple<float,char,float,char,float> inTime()
 int main()
 {
 
-    auto [hour,c1, mine,c2,seco] = inTime();
-    std::cout << hour << c1 << mine << c2 << seco;
+    // float h= std::get<0>(inTime());
+    // char c = std::get<1>(inTime()); 
+    // float m = std::get<2>(inTime()); 
+    // float s = std::get<4>(inTime()) ;
+    // std::cout << h << c << m << c << s; 
 
+    // while (true)
+    // {
+    //     auto [hour,c1, mine,c2,seco] = inTime();
+    //     std::cout << hour << c1 << mine << c2 << seco << std::endl;
+    //     system("clear");
+
+    // }
+
+        std::cout << std::get<0>(inTime())  << std::get<1>(inTime()) 
+        << std::get<2>(inTime())  << std::get<3>(inTime()) 
+        << std::get<4>(inTime()) ;
+
+        std::cout << std::endl;
+        sleep(2);   
+        std::cout << std::get<0>(inTime())  << std::get<1>(inTime()) 
+        << std::get<2>(inTime())  << std::get<3>(inTime()) 
+        << std::get<4>(inTime()) ;
 }
