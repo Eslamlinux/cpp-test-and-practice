ae4d53d335e0fc410d7c1cabe5d6e8883f462d1f
Eslamlinux <linuxeslam@gmail.com>
2025-12-20 10:03:05 +0200

    date formating

--- date_formating.cpp
+++ date_formating.cpp
@@ -2,10 +2,20 @@
 
 #include <iostream>
 #include <ctime>
+#include <sstream>
 
 std::string date_formating(std::string date) 
 { 
+    std::stringstream ss;
+    time_t time_now = time(nullptr);
+    struct tm *rightnow = localtime(&time_now);
+    std::string y , m, d;
+    ss << date;
+    std::getline(ss,y,'-') ;
+    std::getline(ss,m,'-') ;
+    std::getline(ss,d,'-') ;
 
+    return d + '-' +m + '-' + y ;
 }
 
 std::string time_now()
@@ -22,10 +32,15 @@ int main()
     //     system("clear");
     // }
     
-    time_t tim = time(nullptr);
+    // time_t tim = time(nullptr);
 
-    struct tm *local_time = localtime(&tim);
-    std::cout << local_time->tm_year << std::endl;
+    // struct tm *local_time = localtime(&tim);
+    // std::cout << local_time->tm_year + 1900;
+    // std::cout << "-" << local_time->tm_mon+1 ;
+    // std::cout << "-" << local_time->tm_mday << std::endl;
+    // local_time->tm_year = 2024;
+    // std::cout << local_time->tm_year;
 
+    std::cout << date_formating("2020-01-01") <<std::endl;
     return 0;
 }
