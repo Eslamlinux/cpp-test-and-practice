8a8cdef7c699979113000a7f55877408b238281a
Eslamlinux <linuxeslam@gmail.com>
2025-12-20 12:39:39 +0200

    test time and date in ctim lib

--- timeanddate1.cpp
+++ timeanddate1.cpp
@@ -19,21 +19,21 @@ int main()
     // float s = std::get<4>(inTime()) ;
     // std::cout << h << c << m << c << s; 
 
-    // while (true)
-    // {
-    //     auto [hour,c1, mine,c2,seco] = inTime();
-    //     std::cout << hour << c1 << mine << c2 << seco << std::endl;
-    //     system("clear");
+    while (true)
+    {
+        auto [hour,c1, mine,c2,seco] = inTime();
+        std::cout << hour << c1 << mine << c2 << seco << std::endl;
+        system("clear");
+        
+    }
 
-    // }
+        // std::cout << std::get<0>(inTime())  << std::get<1>(inTime()) 
+        // << std::get<2>(inTime())  << std::get<3>(inTime()) 
+        // << std::get<4>(inTime()) ;
 
-        std::cout << std::get<0>(inTime())  << std::get<1>(inTime()) 
-        << std::get<2>(inTime())  << std::get<3>(inTime()) 
-        << std::get<4>(inTime()) ;
-
-        std::cout << std::endl;
-        sleep(2);   
-        std::cout << std::get<0>(inTime())  << std::get<1>(inTime()) 
-        << std::get<2>(inTime())  << std::get<3>(inTime()) 
-        << std::get<4>(inTime()) ;
+        // std::cout << std::endl;
+        // sleep(2);   
+        // std::cout << std::get<0>(inTime())  << std::get<1>(inTime()) 
+        // << std::get<2>(inTime())  << std::get<3>(inTime()) 
+        // << std::get<4>(inTime()) ;
 }
