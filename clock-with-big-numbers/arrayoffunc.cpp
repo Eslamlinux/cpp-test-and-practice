126f8924259914db9eaa25875aeb8b403871f58a
Eslamlinux <linuxeslam@gmail.com>
2025-12-22 18:01:37 +0200

    test play with poiner func

--- clock-with-big-numbers/arrayoffunc.cpp
+++ clock-with-big-numbers/arrayoffunc.cpp
@@ -19,14 +19,14 @@ std::string p3()
 
 std::string p4(int i)
 {
-    return "p3\n";
+    return "p3 num is: " + std::to_string(i);
 }
 
 std::string (* aarfun[])() ={p1,p3};
 
 std::vector<std::function<std::string()>> func ={p1,p3};
 
-std::vector<std::function<void()>> func_def_type ;
+std::vector<std::function<std::string()>> func_def_type ;
 
 int main()
 {
@@ -36,7 +36,13 @@ int main()
     std::cout << func[0]()  << std::endl;
     std::cout << func[0]()  << std::endl;
 
-
+    func_def_type.push_back([](){return p2("eslam");});
+    func_def_type.push_back([](){return p4(10);});
+    
+    for(auto& c: func_def_type)
+    {
+        std::cout << c();
+    }
 
     return 0;
 }
