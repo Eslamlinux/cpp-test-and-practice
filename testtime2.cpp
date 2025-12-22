7449ac997a50784c69fed0882caa68a9b4ee0768
Eslamlinux <linuxeslam@gmail.com>
2025-12-22 02:22:48 +0200

    play with time

--- testtime2.cpp
+++ testtime2.cpp
@@ -14,16 +14,46 @@ std::string Time_Now()
 
 struct BigNumber
 {
-    static std::vector<std::string> zero()
+    static std::vector<std::string> get0()
     {
         return {"  **  ", " *  * ", " *  * ", " *  * ", "  **  "};
     }
-    static std::vector<std::string> One() {
+    static std::vector<std::string> get1() 
+    {
         return {"   * ", "  ** ", "   * ", "   * ", "  *** "};
+    }
+    static std::vector<std::string> get2() 
+    {
+        return {" **** ", "     *", "  *** ", " * ", " *****"};
+    }
+    static std::vector<std::string> get3() 
+    {
+        return {" **** ", "     *", "  *** ", "     *", " **** "};
+    }
+    static std::vector<std::string> get4() {
+        return {" * * ", " * * ", " **** ", "    * ", "    * "};
+    }
+    static std::vector<std::string> get5() {
+        return {" **** ", " * ", " *** ", "    * ", " **** "};
+    }
+    static std::vector<std::string> get6() {
+        return {"  ** ", " * ", " *** ", " * * ", "  ** "};
+    }
+    static std::vector<std::string> get7() {
+        return {" **** ", "    * ", "   * ", "  * ", " * "};
+    }
+    static std::vector<std::string> get8() {
+        return {"  ** ", " * * ", "  ** ", " * * ", "  ** "};
+    }
+    static std::vector<std::string> get9() {
+        return {"  ** ", " * * ", "  *** ", "    * ", "  ** "};
+    }
+    static std::vector<std::string> getColon() {
+        return {"      ", "  * ", "      ", "  * ", "      "};
     }
 
     using FuncNum = std::vector<std::string>(*)();
-    static inline FuncNum funcArr[] = {zero ,One};
+    static inline FuncNum funcArr[] = {get0 ,get1,get2,get3,get4,get5,get6,get7,get8,get9};
 
     std::vector<std::string> printing(int num,int much)
     {
