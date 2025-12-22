39d6ea2d3fbea579c361627e1b42979a78cf5ff5
Eslamlinux <linuxeslam@gmail.com>
2025-12-22 16:34:52 +0200

    test ctime lib

--- clock-with-big-numbers/arrayoffunc.cpp
+++ clock-with-big-numbers/arrayoffunc.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
+#include <functional> 
 
 std::string p1()
 {
     return "p1\n";
 }
-std::string p2()
+std::string p2(std::string str)
 {
-    return< "p2\n";
+    return "p2  hello " + str +"\n";
 }
 
 std::string p3()
@@ -15,10 +16,27 @@ std::string p3()
     return "p3\n";
 }
 
-static std::vector<void> aarfun[] ={p1,p2,p3};
+
+std::string p4(int i)
+{
+    return "p3\n";
+}
+
+std::string (* aarfun[])() ={p1,p3};
+
+std::vector<std::function<std::string()>> func ={p1,p3};
+
+std::vector<std::function<void()>> func_def_type ;
 
 int main()
 {
+    // const std::string tr = aarfun[0];
+    // std::cout << aarfun[0]  << std::endl;
+    
+    std::cout << func[0]()  << std::endl;
+    std::cout << func[0]()  << std::endl;
+
+
 
     return 0;
 }
