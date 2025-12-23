09a9bb0cdf6054deea63eb56c6aed7efff052d2d
Eslamlinux <linuxeslam@gmail.com>
2025-12-24 00:16:51 +0200

    array of ptr test

---
+++ clock-with-big-numbers/arrayptr.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main()
+{
+
+    int* arrint[5];
+
+    *arrint[0] = 10;
+    *arrint[0] = 20;
+    *arrint[0] = 30;
+    *arrint[0] = 40;
+    *arrint[0] = 50;
+
+    return 0;
+}
