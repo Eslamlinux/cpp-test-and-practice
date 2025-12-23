b318597201bc3946a59960bccd4edaa2bfe7ba4b
Eslamlinux <linuxeslam@gmail.com>
2025-12-23 06:03:37 +0200

    test ctime lib

--- clock-with-big-numbers/reap.cpp
+++ clock-with-big-numbers/reap.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
+// insted for loob implemant reap
+#define reap(x) for(int i=0 ,s = x; i < s; i++)
 int main()
 {
 
+ reap(5)
+{
+  
+        std::cout << "Hello Reap \n";
 
+}
 
     return 0;
 }
