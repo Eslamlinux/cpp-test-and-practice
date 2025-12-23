5be59d06b73ed5d451d12db4fbb8352c13dd8965
Eslamlinux <linuxeslam@gmail.com>
2025-12-24 00:10:43 +0200

    array of ptr test

--- clock-with-big-numbers/rrayoffunc3.cpp
+++ clock-with-big-numbers/rrayoffunc3.cpp
@@ -9,17 +9,23 @@ int def(int a,int b)
 {
     return a / b;
 }
+int m(int a,int b){return a * b;}
 
 
 int (*ptrtosum)(int,int) = sum ;
 int (*ptrtodef)(int,int) = def ;
+int (*ptrtomult)(int,int) = m ;
+
+#define mult ptrtomult  //define to the ptr
+
 
 int main()
 {
 
     int res = ptrtosum(5,5);
     std::cout << res <<std::endl; 
-    res = ptrtodef(20,5);
+    res = ptrtodef(20,4);
     std::cout << res <<std::endl; 
+    std::cout << mult(5,5);
 
 }
