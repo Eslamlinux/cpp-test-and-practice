1a8654eb5fc744d5618cbf03372801811b6f30e4
Eslamlinux <linuxeslam@gmail.com>
2025-12-24 08:23:46 +0200

    make array

--- clock-with-big-numbers/arrayptr.cpp
+++ clock-with-big-numbers/arrayptr.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 
+class Array
+{
+    private:
+    int size, length;
+    int* items;
+    public:
+    Array(int siz)
+    {
+        size = siz;
+        length = 0;
+        items = new int[size];
+    }
+    void push_into(int  entry)
+    {
+        static int i =0;
+        items[i] = entry;
+        i++;
+    }
+    int print(int toprint)
+    {
+        return items[toprint];
+    }
+};
 
 int main()
 {
-
-    int* arrint[5];
-
-    *arrint[0] = 10;
-    *arrint[0] = 20;
-    *arrint[0] = 30;
-    *arrint[0] = 40;
-    *arrint[0] = 50;
-
+    Array a1(5);
+    a1.push_into(10);
+    a1.push_into(20);
+    std::cout << a1.print(0) << std::endl;
+    std::cout << a1.print(1) << std::endl;
+    std::cout << a1.print(0) << std::endl;
+    
     return 0;
 }
