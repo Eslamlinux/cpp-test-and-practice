8218c970a84d202fc65e47d097233a332206482f
Eslamlinux <linuxeslam@gmail.com>
2025-12-25 18:01:55 +0200

    array of funt ptr

--- clock-with-big-numbers/arrayoffunc3.cpp
+++ clock-with-big-numbers/arrayoffunc3.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 #include <string>
-
-int* arrfunc(int ar[],int ar2[])
+int siz ;
+struct arr
 {
-    int* sumoftow = new int[sizeof(ar)/sizeof(ar[0])];
-    int length =0 ;
+    int* sumoftow ;
+    arr()
+    {
+        sumoftow = new int[siz];
+    }
+ int* arrfunc(int ar[],int ar2[])
+{
+    for(int i = 0 ; i < siz; i++)
+    {
+        sumoftow[i] = ar[i] + ar2[i];  
+    }
     
-    int* begin()= {return sumoftow;}
-    int* end()= {return sumoftow + length;}
-
     return sumoftow;
 }
-
+int* begin(){return sumoftow;}
+int* end(){return sumoftow+ siz;}
+~arr()
+{
+    delete []sumoftow;
+}
+};
 int main()
 {
+    arr e;
     int a[]= {2,5,8};
     int b[]= {2,5,8};
-    for(int c:arrfunc(a,b))
+    siz = sizeof(a)/sizeof(a[0]);
+    e.arrfunc(a,b);
+    for(int c:e)
     {
+        std::cout << c << " ";
         
     }
 
