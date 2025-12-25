18576464c716428457407631fc04718918666b20
Eslamlinux <linuxeslam@gmail.com>
2025-12-25 19:45:54 +0200

    func frome object

--- funcfromobject.cpp
+++ funcfromobject.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 class whoami
 {
@@ -14,13 +15,22 @@ class whoami
 
 int a(whoami a)
 {
-    return a ;
+    return a.i + a.t ;
 }
+
+
+class str
+{
+
+    std::vector<char> chastr;
+
+
+};
 int main()
 {
-    whoami  who_a;
+    whoami  who_a(2,5);
+    std::cout << a(who_a);
 
-    a(who_a);
 
     return 0;
 }
