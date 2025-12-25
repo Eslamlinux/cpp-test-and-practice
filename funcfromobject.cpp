2cd4f63120ff7eaad6bc88b2b082cc92f66f2162
Eslamlinux <linuxeslam@gmail.com>
2025-12-25 18:08:17 +0200

    play with object function

--- funcfromobject.cpp
+++ funcfromobject.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
+class whoami
+{
+    public:
+    int i = 0;
+};
 
+whoami a(whoami a)
+{
+    return a ;
+}
 int main()
 {
+    whoami  who_a;
 
+    a(who_a);
 
     return 0;
 }
