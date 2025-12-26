a9209284791669992ee707f33067d2a60830d438
Eslamlinux <linuxeslam@gmail.com>
2025-12-26 03:00:03 +0200

    func frome object

--- stuff.cpp
+++ stuff.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
 
 struct stuff
+{
+    std::string item_name;
+    std::string describe;
+    std::string made_by;
+    std::string barcode;
+    std::string manufacturing_date;
+    std::string expiration_date;
+    std::string supplier_name;
+    std::string supplier_phone;
+    int item_count;
+    void add_item();
+    void edit_item();
+    void delete_item();
+    void search_item();
+
+};
+struct showing_stuff
+{
+    char entry;
+
+};
 
 int main()
 {
@@ -8,3 +29,13 @@ int main()
 
     return 0;
 }
+
+    void stuff::add_item()
+    {
+        std::cout << "enter the item Name\n";
+        std::getline(std::cin,stuff::item_name);
+        std::cout << "enter the item description\n";
+
+
+    }
+
