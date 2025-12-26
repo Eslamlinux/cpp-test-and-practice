54b541486dafd4003453dad9e2bdd8038dc3aa49
Eslamlinux <linuxeslam@gmail.com>
2025-12-26 16:21:16 +0200

    paly with enum2

--- enum2.cpp
+++ enum2.cpp
@@ -6,18 +6,29 @@ int main()
 {
     std::cout << "==========================================\n" ;
     std::cout << "enter the protocol\n" ;
+    std::cout << "------------------\n" ;
     std::cout << "www [0] \t http [1] \t https [2] \nweb [3] \t httpss [4]" << std::endl ;
     std::cout << "==========================================\n" ;
     int entry ;
-    protocols proto = (protocols)entry;
     std::cin >> entry;
+    protocols proto = (protocols)entry;
 
     switch(proto)
     {
-        case protocols::www:
+        case protocols::www :
         std::cout << "www.website.com\n";
         break;
-
+        case protocols::http:
+        std::cout << "http.website.com\n";
+        break;
+        case protocols::https:
+        std::cout << "https.website.com\n";
+        break;
+        case protocols::httpss:
+        std::cout << "httpss.website.com\n";
+        break;
+        case protocols::web:
+        std::cout << "web.website.com\n";
     }
 
 
