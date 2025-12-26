6475f29cdc44f81a6ccb1b07ba8f615320e24bab
Eslamlinux <linuxeslam@gmail.com>
2025-12-26 08:14:58 +0200

    project stuff items

--- stuff.cpp
+++ stuff.cpp
@@ -12,7 +12,7 @@ struct stuff
     std::string expiration_date;
     std::string supplier_name;
     std::string supplier_phone;
-    int item_count;
+    size_t item_count , item_id_number;
     std::vector<stuff> data;
     int lastdata_index = data.size();
     void add_item();
@@ -26,7 +26,8 @@ struct showing_stuff:stuff
 {
     char entry;
     void main_manu();
-    void print_entry();
+    void print_entry_by_id();
+    void print_all();
     ~showing_stuff();
 };
 
@@ -48,6 +49,16 @@ showing_stuff a;
         system("clear");
         std::cout << "enter the item Name\n";
         std::getline(std::cin,new_data.item_name);
+        std::cout << "enter the item id or number\n";
+        std::cin.ignore(1000,'\n');
+        std::cin.clear();
+        while(!(std::cin>>new_data.item_id_number))
+        {
+            std::cout << "Invalid input! Please enter a NUMBER\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+        }    
+        std::cin.ignore(1000,'\n');
         std::cout << "enter the item description\n";
         std::getline(std::cin,stuff::describe);
         std::cout << "enter the item Made By\n";
@@ -71,7 +82,11 @@ showing_stuff a;
             return;
         } 
         data.push_back(new_data);
-        std::cout << "Item added successfully!\n";
+        if(!new_data.item_name.empty())
+        {
+            system("clear");
+            std::cout << "Item (" << new_data.item_name << ") added successfully!\n";
+        }
 
     }
     void showing_stuff::main_manu()
@@ -92,7 +107,7 @@ showing_stuff a;
             break;
             case 'p':
             std::cout << "======= stuff item =========\n";
-            print_entry();
+            print_all();
             break;
             // case 'e':
             // stuff::edit_item();
@@ -100,21 +115,22 @@ showing_stuff a;
             // case 'd':
             // stuff::delete_item();
             // break;
-            // case 's':
-            // stuff::search_item();
-            // break;
+            case 's':
+            stuff::search_item();
+            break;
             case 'q':
             system("clear");
             std::cout << "bye bye\n";
             break;
 
             default:
+            system("clear");
             std::cout << "wrong entry!!!\n";
             break;
         }
     }
 
-    void showing_stuff::print_entry()
+    void showing_stuff::print_all()
     {
         system("clear");
         for(auto c :data)
@@ -123,6 +139,46 @@ showing_stuff a;
             std::cout << c.item_count << std::endl;
 
         }
+    }
+    void showing_stuff::print_entry_by_id(size_t id)
+    {
+        
+    }
+
+    void stuff::search_item()
+    {
+        char entrynum;
+        std::cout << "search by id press [1] ""that will show the courrect item"" \n";
+        std::cout << "search by name press [2] ""that will show all item have the same name"" \n";
+        std::cin >> entrynum;
+        if(!isdigit(entrynum) || entrynum-'0' > 2 || entrynum-'0' <1)
+        {
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(),'\n');
+            std::cout << "you cant add character or more than 1 or 2\n";
+            return;
+        }
+        std::cin.ignore(1000,'\n');
+        if(entrynum == '1')
+        {
+            int id =0;
+            std::cout << "enter the id to search\n";
+            if(!(std::cin >> id))
+            {
+            std::cout << "you cant add character or more than 1 or 2\n";
+            return;
+            }
+            else
+            for(int i = 0; i < data.size(); i++)
+            {
+                if(data[i].item_id_number == id)
+                {
+
+                }
+
+            }
+
+        }
+        std::cout << "enter item name to search\n";
     }
     stuff::~stuff(){};
     showing_stuff::~showing_stuff(){};
