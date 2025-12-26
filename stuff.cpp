69d66f0f95fd68d1149008ecddc8e42ff46831b9
Eslamlinux <linuxeslam@gmail.com>
2025-12-26 03:32:30 +0200

    project stuff items

--- stuff.cpp
+++ stuff.cpp
@@ -16,16 +16,20 @@ struct stuff
     void delete_item();
     void search_item();
 
+    ~stuff();
 };
-struct showing_stuff
+struct showing_stuff:stuff
 {
+    void main_manu();
     char entry;
+    ~showing_stuff();
 
 };
 
 int main()
 {
-
+    showing_stuff a;
+    a.main_manu();
 
     return 0;
 }
@@ -36,7 +40,51 @@ int main()
         std::getline(std::cin,stuff::item_name);
         std::cout << "enter the item description\n";
         std::getline(std::cin,stuff::describe);
-
+        std::cout << "enter the item Made By\n";
+        std::getline(std::cin,stuff::made_by);
+        std::cout << "enter the item barcode\n";
+        std::getline(std::cin,stuff::barcode);
+        std::cout << "enter the item manufacturing date\n";
+        std::getline(std::cin,stuff::manufacturing_date);
+        std::cout << "enter the item expiration date\n";
+        std::getline(std::cin,stuff::expiration_date);
+        std::cout << "enter the item supplier name\n";
+        std::getline(std::cin,stuff::supplier_name);
+        std::cout << "enter the item supplier phone\n";
+        std::getline(std::cin,stuff::supplier_phone);
+        std::cout << "enter the item count\n";
+        std::cin >> stuff::item_count;
+        std::cin.ignore();
 
     }
+    void showing_stuff::main_manu()
+    {
+        std::cout << "===== wellcome to Stuff item main menu progrram =====\n";
+        std::cout << "to add items press [i] \n";
+        std::cout << "to edit items press [e] \n";
+        std::cout << "to delete items press [d] \n";
+        std::cout << "to search for an items press [s] \n";
+        stuff a;
+        std::cin >> showing_stuff::entry;
+        switch(showing_stuff::entry)
+        {
+            case 'i':
+            stuff::add_item();
+            break;
+            case 'e':
+            stuff::edit_item();
+            break;
+            case 'd':
+            stuff::delete_item();
+            break;
+            case 's':
+            stuff::search_item();
+            break;
+            default:
+            std::cout << "wrong entry!!!\n";
+            break;
+        }
+    }
 
+    stuff::~stuff(){};
+    showing_stuff::~showing_stuff(){};
