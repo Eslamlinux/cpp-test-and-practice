f5920e7f4b1d9420a4918c10cc5905fe6dc3bbb2
Eslamlinux <linuxeslam@gmail.com>
2025-12-26 11:31:10 +0200

    paly with enum

--- enumtest.cpp
+++ enumtest.cpp
@@ -8,13 +8,16 @@ int main()
     int num;
     std::cout << "enter country number\n";
     std::cin >> num;
+    std::cin.clear();
     mideleast = (country)num;
-    if(mideleast = country::Egypt)
+    if(mideleast == country::Egypt)
     std::cout << "wellcome to Egypt\n";
-    else if(mideleast = country::Iraq)
+    else if(mideleast == country::Iraq)
     std::cout << "wellcome to Iraq\n";
-    else if(mideleast = country::Oman)
+    else if(mideleast == country::Oman)
     std::cout << "wellcome to Oman\n";
-    else if(mideleast = country::qatar)
+    else if(mideleast == country::qatar)
     std::cout << "wellcome to Qatar\n";
+    else
+    std::cout << "wellcome to world\n";
 }
