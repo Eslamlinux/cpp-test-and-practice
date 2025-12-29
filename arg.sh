a47685db350b88cf105604a70b1bbeaf546f8b9e
Eslamlinux <linuxeslam@gmail.com>
2025-12-29 18:47:46 +0200

    up

--- arg.sh
+++ arg.sh
@@ -8,8 +8,8 @@ elif [ $# -eq 2 ]; then
   echo "Two arguments provided: $1 and $2"
 elif [ $# -eq 3 ]; then
 #   echo  "third arg:>> $3"
-git add .; git commit -m "up" ;git push
 echo "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"
+git add .; git commit -m "up" ;git push
 else
   echo "More than two arguments provided:"
   echo "First argument: $1"
