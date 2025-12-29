cca264476eeab0bdcdb5bf7e5dfcbe372d81fdeb
Eslamlinux <linuxeslam@gmail.com>
2025-12-29 10:45:25 +0200

    Multiplication table from 1 to 10

--- 10multiplication.cpp
+++ 10multiplication.cpp
@@ -2,18 +2,19 @@
 
 void printhead()
 {
+    std::cout << "\t\t\t\tMultiplication Table From 1 To 10\n\t";
     for(int i = 1; i <= 10;i++)
     {
         std::cout  << "\t"<< i;
     }
-        std::cout << "\n___________________________________________________________________________________\n";
+        std::cout << "\n___________________________________________________________________________________________\n";
 }
 void printtble()
 {
     printhead();
     for(int i = 1; i <= 10;i++)
     {
-        std::cout << i << "\t|  ";
+        std::cout << i << "\t|\t";
         for(int t = 1;t <= 10 ;t++)
         {
             std::cout << i * t << "\t";
