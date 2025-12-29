33307fadb219f8e0af68d15cc3232fca8c0bba00
Eslamlinux <linuxeslam@gmail.com>
2025-12-29 10:29:46 +0200

    multib table 10

--- 10multiblacationtable.cpp
+++ 10multiblacationtable.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+void printhead()
+{
+    
+}
 void printtble()
 {
     for(int i = 1; i <= 10;i++)
     {
-        for(int t = i+1;t <= 10 ;t++)
+        std::cout << i << "\t|  ";
+        for(int t = 1;t <= 10 ;t++)
         {
             std::cout << i * t << "\t";
         }
@@ -14,6 +19,6 @@ void printtble()
 }
 int main()
 {
-
+    printtble();
     return 0;
 }
