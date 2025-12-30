813e23df1f3a09f3f46f83011985f48a513b3eed
Islam <eslamlinux@gmail.com>
2025-12-30 02:45:41 +0200

    c lang class

---
+++ c_practice/c1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void main()
+{
+	typedef long int lt;
+	typedef signed si;
+	typedef unsigned usi;
+	typedef signed int sii;
+	printf("size of int = %d \n",sizeof(int));
+	printf("size of doble = %d \n",sizeof(double));
+	printf("size of float = %d \n",sizeof(float));
+	printf("size of char= %d \n",sizeof(char));
+	printf("size of long int = %d \n",sizeof(lt));
+	printf("size of long signed = %d \n",sizeof(si));
+	printf("size of long unsigned = %d \n",sizeof(usi));
+	printf("size of long signed int = %d \n",sizeof(sii));
+	//printf("size of bool = %d \n",sizeof(bool));
+
+}
