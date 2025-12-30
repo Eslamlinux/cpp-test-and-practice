9880586389894cf7e8154ecae46cdcbc9dcac35b
Islam <eslamlinux@gmail.com>
2025-12-30 04:12:57 +0200

    struct in c

--- c_practice/structinc.c
+++ c_practice/structinc.c
@@ -15,9 +15,24 @@ int main()
 	str.string[2] = 'l';
 	str.string[3] = 'a';
 	str.string[4] = 'm';
-	printf("string = %c \n", str.string[0]);
+
+	printf("\nstring = %c \n", str.string[0]);
 	printf("string = %c \n", str.string[1]);
 	printf("string = %c \n", str.string[2]);
 	printf("string = %c \n", str.string[3]);
 	printf("string = %c \n", str.string[4]);
+
+	char st[] = {'e','s','l','a','m'};
+	printf("\nst = %c \n", st[0]);
+	printf("st = %c \n", st[1]);
+	printf("st = %c \n", st[2]);
+	printf("st = %c \n", st[3]);
+	printf("st = %c \n", st[4]);
+
+	char name[] = "eslam";
+	for(int i = 0; i < name.size();i++)
+	{
+	printf("name = %c \n", name[i]);
+	}
+	
 }
