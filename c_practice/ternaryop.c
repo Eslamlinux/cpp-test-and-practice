93bfff13a25fe5243913ee61075bae83d520e368
Eslamlinux <linuxeslam@gmail.com>
2025-12-31 01:27:02 +0200

    c test ternary

--- c_practice/ternaryop.c
+++ c_practice/ternaryop.c
@@ -5,6 +5,8 @@ void main()
     int a,b;
     printf("enter a tow num\n");
     scanf("%d%d",&a,&b);
+    if(a <= 0||b <= 0)
+    return;
     float res = a > b?a/b:b/a;
 
     printf("result %d / %d =: %f",a,b,res);
