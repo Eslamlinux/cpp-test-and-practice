f5541acb22dd8ea54c324d95b1967439833db10a
Islam <eslamlinux@gmail.com>
2025-12-30 02:52:31 +0200

    c enum in c

--- c_practice/c1.c
+++ c_practice/c1.c
@@ -5,7 +5,6 @@ void main()
 	typedef long int lt;
 	typedef signed si;
 	typedef unsigned usi;
-	typedef signed int sii;
 	printf("size of int = %d \n",sizeof(int));
 	printf("size of doble = %d \n",sizeof(double));
 	printf("size of float = %d \n",sizeof(float));
@@ -13,7 +12,6 @@ void main()
 	printf("size of long int = %d \n",sizeof(lt));
 	printf("size of long signed = %d \n",sizeof(si));
 	printf("size of long unsigned = %d \n",sizeof(usi));
-	printf("size of long signed int = %d \n",sizeof(sii));
 	//printf("size of bool = %d \n",sizeof(bool));
 
 }
---
+++ c_practice/c2enum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+void main()
+{
+	typedef enum
+	{
+		day1 =0,
+		day2,
+		day3,
+		day4,
+		day5,
+		day6,
+		day7
+	}days;
+
+}
