7970694becefa8fc5725c39d3bddd4bb1d226c2f
Eslamlinux <linuxeslam@gmail.com>
2025-12-31 17:43:40 +0200

    conditional compilation in c

--- c_practice/c4.c
+++ c_practice/c4.c
@@ -3,11 +3,16 @@
 
 void main()
 {
-    char str[] = "string\n";
-
+    char str[] = "string1";
+    char str2[] = " string2";
     int len = strlen(str);
 
-    printf("string length = %d\n",len);
+    printf("string 1 length = %d\n",len);
+
+    strcat(str,str2);
+    printf("%s\n",str);
+    printf("%s\n",str2);
+    printf("string 1 & 2 length after concat: %d\n",strlen(str));
 
 
 }
