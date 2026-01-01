b61bd15b3e3c1b866019fa4b51416314ff317a07
Eslamlinux <linuxeslam@gmail.com>
2026-01-01 07:48:03 +0200

    macro functions

--- macro_functions.cpp
+++ macro_functions.cpp
@@ -5,12 +5,21 @@ void func_##name() { \
 std::cout<< "func: " << "wellcome from " #name << std::endl;\
 }
 
+#define make_vall(v) int vall_##v
+#define vnum(i) vall_##i
+
 make_func(func1);
 
 int main()
 {
 
     func_func1();
+    make_vall(count);
+
+    vall_count = 10;
+    std::cout << vall_count << std::endl;
+
+
 
 
 }
