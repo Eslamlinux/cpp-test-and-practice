9846c029b0b7efdea59138789d9f9783258f37d3
Eslamlinux <linuxeslam@gmail.com>
2026-01-01 04:30:11 +0200

    conditional compilation in cpp

--- ctypeincpp.cpp
+++ ctypeincpp.cpp
@@ -1,15 +1,29 @@
 #include <iostream>
 
-#define Error 1
+#define Error 0
 
 
 int main()
 {
+    
     #ifdef __linux__
     std::cout << "linux on\n";
+        #if Error == 1
+        std::cout << "Status: Error\n";
+        #else
+        std::cout << "Status: Healthy\n";
+        #endif
+    std::cout << "=== youre sys info ===\n";
+    std::cout << system("uname -a");
+    std::cout <<std::endl;
+    std::cout << "=== youre user info ===\n";
+    std::cout << system("id");
+    std::cout <<std::endl;
+    
     
     #elif defined(_WIN32) || defined(_WIN64)
     std::cout << "win on\n";
+
     
     #else
     std::cout << "othe sys\n";
@@ -19,12 +33,6 @@ int main()
     // #ifndef Error
     // #define Error 1
     // #endif
-
-    #if Error == 1
-    std::cout << "error\n";
-    #else
-    std::cout << "Status: Healthy\n";
-    #endif
 
 
     return 0;
