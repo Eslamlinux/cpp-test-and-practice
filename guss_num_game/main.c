2c68149f5388f9f790ee38d99241c5a17699245c
Eslamlinux <linuxeslam@gmail.com>
2026-01-02 04:20:36 +0200

    guss game

--- guss_num_game/main.c
+++ guss_num_game/main.c
@@ -8,11 +8,11 @@
 #define Board_Size 8
 #define Tile_Size 42
 #define Tile_Types 5
-
-//#define screen_width 800
-//#define screen_height 650
-const int screen_width = 800;
-const int screen_height = 650;
+#define MAX_SCORE_POPUPS 32
+#define screen_width 800
+#define screen_height 650
+//const int screen_width = 800;
+//const int screen_height = 650;
 
 const char tile_chars[Tile_Types] = {'#','@','$','%','&'};
 
@@ -24,6 +24,17 @@ int Score = 200;
 float fall_speed = 8.0f;
 float match_delay_timer = 0.0f;
 const float MATCH_DELAY_DURATION = 0.2f;
+Vector2 grid_origin;
+Texture2D background;
+Font Score_Font;
+Vector2 selected_tile = {-1,-1};
+
+float score_scale = 1.0f;
+float score_scale_velocity = 0.0f;
+bool score_animating = false;
+
+Music background_music;
+Sound match_sound;
 
 typedef enum{
     STATE_IDLE,
@@ -32,6 +43,17 @@ typedef enum{
 }Tile_State;
 
 Tile_State tile_state;
+
+
+typedef struct {
+	Vector2 position;
+	int amount;
+	float lifetime;
+	float alpha;
+	bool active;
+} ScorePopup;
+ScorePopup score_popups[MAX_SCORE_POPUPS] = { 0 };
+
 
 void swap_tile(int x1,int y1, int x2, int y2)
 {
@@ -42,6 +64,25 @@ void swap_tile(int x1,int y1, int x2, int y2)
 bool are_tiles_adjacent(Vector2 a,Vector2 b){
     return (abs((int)a.x - (int)b.x) + abs((int)a.y - (int)b.y)) == 1;
 }
+
+
+void add_score_popup(int x, int y, int amount, Vector2 grid_origin) {
+	for (int i = 0; i < MAX_SCORE_POPUPS; i++) {
+		if (!score_popups[i].active) {
+			score_popups[i].position = (Vector2){
+				grid_origin.x + x * Tile_Size + Tile_Size / 2,
+				grid_origin.y + y * Tile_Size + Tile_Size / 2
+			};
+			score_popups[i].amount = amount;
+			score_popups[i].lifetime = 1.0f;
+			score_popups[i].alpha = 1.0f;
+			score_popups[i].active = true;
+			break;
+		}
+	}
+}
+
+
 bool find_matches()
 {
     bool found =false;
@@ -63,6 +104,13 @@ bool find_matches()
                 matched[y][x] = matched[y][x+1] = matched[y][x+2] = true;
                 Score += 10;
                 found = true;
+				PlaySound(match_sound);
+
+                score_animating =true;
+                score_scale = 2.0f;
+                score_scale_velocity = -2.5f;
+
+                add_score_popup(x, y, 10, grid_origin);
             } 
         }
     }
@@ -76,6 +124,13 @@ bool find_matches()
                 matched[y][x] = matched[y+1][x] = matched[y+2][x] = true;
                 Score +=10;
                 found = true;
+
+                PlaySound(match_sound);
+				score_animating = true;
+				score_scale = 2.0f;
+				score_scale_velocity = -2.5f;
+				add_score_popup(x, y, 10, grid_origin);
+
              }
          }
      }
@@ -92,16 +147,17 @@ void resolve_matches()
     for(int x =0;x < Board_Size ; x++)
     {
         int write_y = Board_Size - 1;
-        for(int y =0;y < Board_Size -1 ; y--)
+        for(int y =Board_Size -1;y >=0 ; y--)
         {
             if(!matched[y][x]){
+                if(y !=write_y){
                 board[write_y][x] = board[y][x];
                 fall_offset[write_y][x] = (write_y - y) * Tile_Size;
                 board[y][x] = ' ';
             }
                 write_y--;
         }
-
+    }
         while(write_y >= 0){
             board[write_y][x] = random_tile();
             fall_offset[write_y][x] = (write_y + 1) * Tile_Size;
@@ -111,13 +167,7 @@ void resolve_matches()
     tile_state = STATE_ANIMATING;    
 }
 
-Vector2 grid_origin;
 
-Texture2D background;
-
-Font Score_Font;
-
-Vector2 selected_tile = {-1,-1};
 
 void init_board()
 {
@@ -149,15 +199,28 @@ int main(void)
 	InitWindow(screen_width, screen_height,"guss number game");
 	SetTargetFPS(60);
 	srand(time(NULL));
+
+    InitAudioDevice();
     
     background = LoadTexture("assets/bg.png");
-Score_Font = LoadFontEx("assets/BoldPixelsFont.ttf",Score_Font_Size, NULL,0);
+    Score_Font = LoadFontEx("assets/BoldPixelsFont.ttf",Score_Font_Size, NULL,0);
+	background_music = LoadMusicStream("assets/sound.mp3");
+	match_sound = LoadSound("assets/right.mp3");
 
+	PlayMusicStream(background_music);
+
+	SetMusicVolume(background_music, 0.60f);
+
+	SetSoundVolume(match_sound, 0.40f);
+
+    
     init_board();
     Vector2 mouse = {0,0};
 
 	while(!WindowShouldClose())
 	{
+        UpdateMusicStream(background_music);
+        
         mouse = GetMousePosition();
 
         if(tile_state == STATE_IDLE && IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
@@ -184,11 +247,6 @@ Score_Font = LoadFontEx("assets/BoldPixelsFont.ttf",Score_Font_Size, NULL,0);
                      }
             }
         }
-       /* if(find_matches())
-        {
-            resolve_matches();
-        }
-        */
 
         if(tile_state == STATE_ANIMATING){
             bool still_animating =false;
@@ -224,6 +282,26 @@ Score_Font = LoadFontEx("assets/BoldPixelsFont.ttf",Score_Font_Size, NULL,0);
                         }
                         }
                         }
+
+        		for (int i = 0; i < MAX_SCORE_POPUPS; i++) {
+			if (score_popups[i].active) {
+				score_popups[i].lifetime -= GetFrameTime();
+				score_popups[i].position.y -= 30 * GetFrameTime();
+				score_popups[i].alpha = score_popups[i].lifetime;
+
+				if (score_popups[i].lifetime <= 0.0f) {
+					score_popups[i].active = false;
+				}
+			}
+		}
+
+		if (score_animating) {
+			score_scale += score_scale_velocity * GetFrameTime();
+			if (score_scale <= 1.0f) {
+				score_scale = 1.0f;
+				score_animating = false;
+			}
+		}
 
 		BeginDrawing();
 		ClearBackground(BLUE);
@@ -284,13 +362,30 @@ Score_Font = LoadFontEx("assets/BoldPixelsFont.ttf",Score_Font_Size, NULL,0);
                 Score_Font,
                 TextFormat("SCORE: %d",Score),
                 (Vector2) {40,20},
-               Score_Font_Size,1.0f ,RED
+               Score_Font_Size * score_scale ,1.0f,RED
                );
-		EndDrawing();
+
+
+				for (int i = 0; i < MAX_SCORE_POPUPS; i++) {
+			if (score_popups[i].active) {
+				Color c = Fade(YELLOW, score_popups[i].alpha);
+				DrawText(
+					TextFormat("+%d", score_popups[i].amount),
+					score_popups[i].position.x,
+					score_popups[i].position.y,
+					20, c);
+			}
+		}
+
+        EndDrawing();
 	}
     UnloadTexture(background);
-	CloseWindow();
+	StopMusicStream(background_music);
+	UnloadMusicStream(background_music);
+	UnloadSound(match_sound);
     UnloadFont(Score_Font);
-	return 0;
+	CloseAudioDevice();
+    CloseWindow();
+    return 0;
 }
 
