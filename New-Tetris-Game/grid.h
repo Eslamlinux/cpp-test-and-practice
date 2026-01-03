cc4d014da800e95b59795dab124fb4f4202f1c17
Eslamlinux <linuxeslam@gmail.com>
2026-01-03 10:08:30 +0200

    make new Tetris game with raylib

---
+++ New-Tetris-Game/grid.h
@@ -0,0 +1,14 @@
+#pragma once
+
+class Grid
+{
+    int num_rows;
+    int num_cols;
+    int cell_size;
+
+    public:
+    Grid();
+    int grid[20][10];
+    void initialize();
+    void print();
+};
---
+++ New-Tetris-Game/main.cpp
@@ -0,0 +1,24 @@
+#include <raylib.h>
+#define G_Hight_Y 500
+#define G_Width_X 600
+#define Main_Title "test game"
+
+
+int main()
+{
+    InitWindow(G_Width_X,G_Hight_Y,Main_Title); // النافذذة الرئسية
+    Color background = {5, 240, 0, 44}; // {255,0,0,255} يمكن استخدام اسم اللون او الار جي بي الخاص باللون  
+    SetTargetFPS(60); // سرعة اللعبة
+    while(!WindowShouldClose())
+    {
+        BeginDrawing(); //لبداية رسم النافذة
+        ClearBackground(background); // لتنظيف واضافة خلفية جديدة
+
+        EndDrawing(); // لانهاء الرسم وتدمير مكونات الرسم
+
+
+    }
+
+    CloseWindow(); //لاغلاق وتدمير النافذة
+    return 0;
+}
