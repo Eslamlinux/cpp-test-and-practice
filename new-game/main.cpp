91c27a7b8b141bbcac9b49d60eff025379d9935f
Eslamlinux <linuxeslam@gmail.com>
2026-01-03 06:46:24 +0200

    make new game with raylib

--- new-game/main.cpp
+++ new-game/main.cpp
@@ -1,12 +1,19 @@
 #include <raylib.h>
 #define G_Hight_Y 500
 #define G_Width_X 600
-
+#define Main_Title "test game"
 
 
 int main()
 {
-    InitWindow()
+    InitWindow(G_Width_X,G_Hight_Y,Main_Title); // النافذذة الرئسية
+    SetTargetFPS(60); // سرعة اللعبة
+    while(!WindowShouldClose)
+    {
+
+
+
+    }
 
 
     return 0;
