f6c4febc6e70770c77cdfd69049261e52b9d5be5
Eslamlinux <linuxeslam@gmail.com>
2026-01-03 11:51:09 +0200

    make new Tetris game with raylib

--- New-Tetris-Game/grid.h
+++ New-Tetris-Game/grid.h
@@ -1,11 +1,13 @@
 #pragma once
-
+#include <raylib.h>
+#include <vector>
 class Grid
 {
     int num_rows;
     int num_cols;
     int cell_size;
-
+    std::vector<Color>GetCellColor();
+    std::vector<Color>Colors;
     public:
     Grid();
     int grid[20][10];
