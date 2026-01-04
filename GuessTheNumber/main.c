34f25a0e4a813a2a978fa1afab15d4cce84cb9d2
Eslamlinux <linuxeslam@gmail.com>
2026-01-04 21:42:10 +0200

    guess game

--- GuessTheNumber/main.c
+++ GuessTheNumber/main.c
@@ -5,21 +5,49 @@
 int random_num(int Max_Num)
 {
     int num;
-    rand() % Max_Num+1;
+    num = rand() % Max_Num+1;
     return Max_Num;
 }
 int main()
 {
     int balance, bet, guess, dice;
     srand(getpid());
-    balnce = 1000;
-    printf("make your bet");
+    balance = 1000;
+    while(1)
+    {
+    printf("give some of your point: ");
     fflush(stdout);
     scanf("%d",&bet);
-    printf("make your guss (1-6, and 0 means quit): \n");
+    printf("make your guess (1-6, and 0 means quit): \n");
+    scanf("%d", &guess);
     if(guess == 0)
         return 0;
+    printf("throwing the dice ...\n");
+    sleep(2);
+    dice = random_num(6);
+    printf("Dice result:  %d\n",dice);
+    
+    if(guess != dice)
+    {
+        balance -= bet;
+    printf("sorry you lost\n");
+    }
+    else
+    {
+        bet *= 3;
+        balance += bet;
+        printf("you won congrats\n");
+    }
 
+    if(balance == 0)
+    {
+        printf("Game Over\n");
+        return 0;
+    }
+
+    printf("balnce : %d points\n" ,balance);
+    }
 
     return 0;
+    
 }
