95360c7fb9f023e2a43803c251ff9bc3df38aff6
Eslamlinux <linuxeslam@gmail.com>
2026-01-06 20:44:49 +0200

    struct in c

--- c_practice/struct.c
+++ c_practice/struct.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-    struct persons
+#define struct persons
 {
     char Name[10];
     char Last_Name[10];
@@ -10,13 +10,13 @@
 
 int main()
 {
-    struct persons eslam;
+     persons eslam;
     strncpy(eslam.Name,"Eslam",6);
     strncpy(eslam.Last_Name,"Khalid",7);
-   eslam.age = 34;
+    eslam.age = 34;
 
 
-  printf("First Name: %s \n Last Name: %s \n age: %d ",eslam.Name, eslam.Last_Name, eslam.age);
+  printf("First Name: %s \nLast Name: %s \nage: %d ",eslam.Name, eslam.Last_Name, eslam.age);
 
 
     return 0;
