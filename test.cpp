2b823f6d9bd972090705eec149cf7c62df085d73
Eslamlinux <linuxeslam@gmail.com>
2026-01-09 23:50:50 +0200

    up

--- test.cpp
+++ test.cpp
@@ -2,8 +2,12 @@
 
 int main()
 {
-    
- std::cout << "hello forme fresh\n";
+
+
+
+ *std::cout << "hello forme fresh\n";
+ *std::cout << "hello forme fresh\n";
+ *std::cout << "hello forme fresh\n";
     
     return 0;
  }
