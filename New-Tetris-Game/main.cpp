b683dd58b5cf8c30e9da376881a6b72ec7d49cc2
Eslamlinux <linuxeslam@gmail.com>
2026-01-16 08:20:27 +0200

    objet set move

--- New-Tetris-Game/main.cpp
+++ New-Tetris-Game/main.cpp
@@ -1,7 +1,5 @@
 #include <raylib.h>
-#include "grid.h"
-#include "block.h"
-#include "blocks.cpp"
+#include "game.h"
 
 #define G_Hight_Y 600
 #define G_Width_X 300
@@ -12,20 +10,15 @@ int main()
     InitWindow(G_Width_X,G_Hight_Y,Main_Title); // النافذذة الرئسية
     Color background = {128,128,128,255}; // {255,0,0,255} يمكن استخدام اسم اللون او الار جي بي الخاص باللون  
     SetTargetFPS(60); // سرعة اللعبة
-Grid grid;
 
-
-grid.print();
-
-IBlock block = IBlock();
 
 // block.Move(4,3);
 while(!WindowShouldClose())
     {
         BeginDrawing(); //لبداية رسم النافذة
         ClearBackground(background); // لتنظيف واضافة خلفية جديدة
-        grid.Draw();
-        block.Draw();
+        Game.Draw();
+
         EndDrawing(); // لانهاء الرسم وتدمير مكونات الرسم
 
 
