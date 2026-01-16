b7f0b8d9c66b506dcfb235a5a3502b04f1c03056
Eslamlinux <linuxeslam@gmail.com>
2026-01-16 17:12:32 +0200

    first ball

---
+++ ballgame/main.cpp
@@ -0,0 +1,42 @@
+#include <raylib.h>
+
+int main()
+{
+    InitWindow(800,600,"Ball");
+    SetTargetFPS(60);
+    int ballx  = 400;
+    int bally  = 400;
+
+    while(!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(BLUE);
+
+        if(IsKeyDown(KEY_UP))
+        {
+            bally-=1;
+        }
+        else if(IsKeyDown(KEY_DOWN))
+        {
+            bally +=1;
+        }
+        else if(IsKeyDown(KEY_LEFT))
+        {
+            ballx-=1;
+        }
+        else if(IsKeyDown(KEY_RIGHT))
+        {
+            ballx +=1;
+        }
+        DrawCircle(ballx,bally,20,WHITE);
+
+        
+
+        EndDrawing();
+
+    }
+
+    
+    CloseWindow();
+
+}
