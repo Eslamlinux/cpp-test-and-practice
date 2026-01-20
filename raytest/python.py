92da7620049a9871a00b3bebcbe36441bbc224d2
Eslamlinux <linuxeslam@gmail.com>
2026-01-20 18:55:43 +0200

    up

--- raytest/python.py
+++ raytest/python.py
@@ -6,3 +6,25 @@ print("Hello python 3")
 #comm 
 #jj
 #k
+
+msg1 = "Hello frme frist msg"
+msg1 = 10
+msg2 = 20
+msg3 = 'eslam'
+msg4 = "eslam"
+# msg3 = "10"
+
+tu = False
+
+if tu:
+    print("okay")
+
+print(5+9)
+
+print(msg3)
+print(msg3 + msg4)
+# print(msg1 + msg2 + msg3)
+
+name, age, id  = "eslam", 33, 105
+
+print(name ," age: ", age, id )
