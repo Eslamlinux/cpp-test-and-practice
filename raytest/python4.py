5cb95d3c55ede0ac979b4d50ddda5da43a4c1bec
Eslamlinux <linuxeslam@gmail.com>
2026-01-21 18:52:32 +0200

    test python

---
+++ raytest/python4.py
@@ -0,0 +1,11 @@
+def sum(a,b):
+    return a + b
+
+def ask_to_sum():
+    print("welcome to sum prog")
+    e1 = input("enter first num: ")
+    e2 = input("enter seco num: ")
+    print(sum(int(e1),int(e2)))
+
+
+ask_to_sum()
