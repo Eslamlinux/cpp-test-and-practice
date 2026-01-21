12bc54ec8acaecb0b322a1912e99b960ac63bf07
Eslamlinux <linuxeslam@gmail.com>
2026-01-21 13:50:45 +0200

    test python

--- raytest/python2.py
+++ raytest/python2.py
@@ -1,6 +1,31 @@
 
 
-num1 = input("enter num 1: ")
-num2 = input("enter num 2: ")
-sum = num1 + num2
-print(type(sum))
+# num1 = input("enter num 1: ")
+# num2 = input("enter num 2: ")
+# sum = int(num1) + int(num2)
+
+# print(sum)
+
+# comp = 10J
+# print(type(comp))
+
+str = "hello"
+print(str.find('l'))
+
+bol = 1
+b = bool(bol)
+print(b)
+
+lis = ["hello", "mr","eslam",100,'h']
+
+print(lis)
+print(lis[0]," ",lis[1]," ",lis[2])
+lis[2]= "semsem"
+print(lis[0],lis[1],lis[2])
+
+lis.append("world")
+print(lis)
+lis.insert(0,"first")
+print(lis)
+
+
