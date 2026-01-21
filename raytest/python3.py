d30d2eccd07abd4cf65765d89a9d0787294c8630
Eslamlinux <linuxeslam@gmail.com>
2026-01-21 18:13:20 +0200

    test python

--- raytest/python3.py
+++ raytest/python3.py
@@ -1,6 +1,12 @@
 
 age = 20
 
-if age >= 19:
-    print("age is good")
-    print("your age is: ",age)
+if age > 18:
+    if age > 20:
+        print("age is good")
+        print("your age is: ",age)
+    elif age >18 and age <21:
+        print("age is cool")
+        print("your age is: ",age)
+else :
+    print("the age not good")
