ffecd7f2a48f4c7785dbb6037b8e9cc4d9e1b319
Eslamlinux <linuxeslam@gmail.com>
2026-01-21 18:38:10 +0200

    test python

--- raytest/python3.py
+++ raytest/python3.py
@@ -10,3 +10,15 @@ if age > 18:
         print("your age is: ",age)
 else :
     print("the age not good")
+
+i = 0
+while i < 5:
+    print(i)
+    i+=1
+
+lis = ["eslam", "aya", "hassan", "mohamed"]
+for c in lis:
+    print("name",c)
+
+for n in range(2,10):
+    print(n)
