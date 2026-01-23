203780514fc00e467fc9ec7c636f5bc738d74842
Eslamlinux <linuxeslam@gmail.com>
2026-01-23 21:19:53 +0200

    pyton sciling

--- raytest/python5.py
+++ raytest/python5.py
@@ -9,3 +9,15 @@ print(txt[4:0:-1])
 s = slice(0,5,3)
 
 print(txt[s])
+
+print(txt.index("e"))
+
+print(len(txt))
+print(txt.count("l"))
+
+print('hello' in txt)
+print('h' in txt)
+print('hello' not in txt)
+print('llo' not in txt)
+
+print(txt*2)
