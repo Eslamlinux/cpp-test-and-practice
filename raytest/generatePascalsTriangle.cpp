bb60d2bc9291eb0b243f4043cb707979def19211
Eslamlinux <linuxeslam@gmail.com>
2026-02-06 21:31:06 +0200

    generate Pascals Triangle

--- raytest/generatePascalsTriangle.cpp
+++ raytest/generatePascalsTriangle.cpp
@@ -15,22 +15,26 @@
 1, 4, 6, 4, 1
 
 ]
-
+1 1 1 1 2 1 1 3 1
 */
 
 std::vector<int> generatePascalsTriangle(int numRows) 
 { 
     std::vector<int>result;
-    for(int i = 0; i < numRows;i++)
+    for(int i = 0; i < numRows;i++) // 3
     {
+        result.push_back(1);
         if(i > 0)
         {
-            result.push_back(1);
-            result.push_back(i);
-            result.push_back(1);
+            for(int t = i;t < i+1;t++)
+            {
+                result.push_back(t);
+
+            }
         }
-        else
+        if(i > 1)
         result.push_back(1);
+
     }
     return result;
 }
