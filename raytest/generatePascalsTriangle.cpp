ac77941eda9ff1d6f38f8a44848ceffd95718398
Eslamlinux <linuxeslam@gmail.com>
2026-02-06 22:47:25 +0200

    generate Pascals Triangle

--- raytest/generatePascalsTriangle.cpp
+++ raytest/generatePascalsTriangle.cpp
@@ -18,27 +18,44 @@
 1 1 1 1 2 1 1 3 1
 */
 
-std::vector<int> generatePascalsTriangle(int numRows) 
-{ 
-    std::vector<int>result;
-    for(int i = 0; i < numRows;i++) // 3
-    {
-        result.push_back(1);
-        if(i > 0)
-        {
-            for(int t = i;t < i+1;t++)
-            {
-                result.push_back(t);
+// std::vector<int> generatePascalsTriangle(int numRows) 
+// { 
+//     std::vector<int>result;
+//     for(int i = 0; i < numRows;i++) // 3
+//     {
+//         result.push_back(1);
+//         if(i > 0)
+//         {
+//             for(int t = i;t < i+1;t++)
+//             {
+//                 result.push_back(t);
 
-            }
+//             }
+//         }
+//         if(i > 1)
+//         result.push_back(1);
+
+//     }
+//     return result;
+// }
+
+std::vector<int> generatePascalsTriangle(int numRows) {
+    std::vector<std::vector<int>> triangle;
+    std::vector<int> result;
+
+    for (int i = 0; i < numRows; i++) {
+        std::vector<int> row(i + 1, 1);
+        for (int j = 1; j < i; j++) {
+            row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
         }
-        if(i > 1)
-        result.push_back(1);
-
+        triangle.push_back(row);
+        
+        for (int val : row) {
+            result.push_back(val);
+        }
     }
     return result;
 }
-
 
 int main()
 {
