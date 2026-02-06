161c5c8017328bbab2241bebc53eb5a8e04a92c1
Eslamlinux <linuxeslam@gmail.com>
2026-02-06 23:23:51 +0200

    generate Pascals Triangle

--- raytest/inputtype.py
+++ raytest/inputtype.py
@@ -1,11 +1,18 @@
-from typing import List
-def input_type(value: str) -> str:
-    if value.isalpha():
-        return "string"
-    elif value[0].isdigit():
-        if(value.find('.')):
-            return "double"
-        else:
-            return "integer"
-    return value
+# from typing import List
+# def input_type(value: str) -> str:
+#     if value.isalpha():
+#         return "string"
+#     elif value[0].isdigit():
+#         if(value.find('.')):
+#             return "double"
+#         else:
+#             return "integer"
+#     return value
+
+class test:
+    def __init__(self,name,id):
+        self.name = name
+        self.id = id
+    def talk(self):
+        print("my name ",self.name)
 
