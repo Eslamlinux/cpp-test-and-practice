a1446cd092844ef4337ffc15889d452f65ca7d39
Eslamlinux <linuxeslam@gmail.com>
2026-02-11 00:00:08 +0200

    port scanner

--- raytest/Port_Scanner/gui_scanner.py
+++ raytest/Port_Scanner/gui_scanner.py
@@ -68,7 +68,7 @@ lbl_result.pack(pady=15)
 
 # زر التشغيل
 btn_scan = tk.Button(root, text="Start Scan", command=run_scanner, 
-                      bg="#2c3e50", fg="white", font=("Arial", 12), width=15)
+                      bg="#2c3e50", fg="white", font=("Arial", 12), width=15,bd=float)
 btn_scan.pack()
 
 btn_about = tk.Button(root, text="About the Programmer", command=run_about, 
