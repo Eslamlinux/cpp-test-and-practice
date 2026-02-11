a872191ba5d7788391f155bba7199665c8e847af
Eslamlinux <linuxeslam@gmail.com>
2026-02-11 13:45:04 +0200

    python app 1

--- raytest/Port_Scanner/pyapp1.py
+++ raytest/Port_Scanner/pyapp1.py
@@ -6,6 +6,13 @@ class vehicle:
         self.model = model
 
     def Print_All_Data(self):
-        print("company name: ",self.company,"Color: ",self.color, "Model: ",self.model)
+        print("company name:",self.company,"\nColor:",self.color, "\nModel:",self.model)
 class car(vehicle):
-    
+    def Tech_Detels(self,Car_Speed,Engin):
+        self.Car_Speed = Car_Speed
+        self.Engin = Engin 
+
+
+first = car("BMW","Blue",8812)
+
+first.Print_All_Data()
