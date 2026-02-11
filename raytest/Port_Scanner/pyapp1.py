c11ca880c9738dacf3fc62609043298a0797dc7d
Eslamlinux <linuxeslam@gmail.com>
2026-02-11 14:05:15 +0200

    python app 1

--- raytest/Port_Scanner/pyapp1.py
+++ raytest/Port_Scanner/pyapp1.py
@@ -13,9 +13,19 @@ class car(vehicle):
         self.Engin = Engin 
     def get_Tech_Detels(self):
         print("speed =",self.Car_Speed,"\nEngin:",self.Engin)
+class truck(vehicle,car):
+    def Tech_Detels(Tyre_size):
+        return Tyre_size
+
 
 first = car("BMW","Blue",8812)
 first.Tech_Detels("20m/m","unknown")
+first_t = truck("BMW","green",231)
+first_t.Tech_Detels("17-14")
 
 first.Print_All_Data()
 first.get_Tech_Detels()
+
+first_t.Print_All_Data()
+first_t.get_Tech_Detels()
+print(first_t.Tech_Detels(17))
