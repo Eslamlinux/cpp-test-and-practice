6d1cefe3d7230bd171145d6025a9ad14facef88d
Eslamlinux <linuxeslam@gmail.com>
2026-02-14 23:21:31 +0200

    global scope and module

--- raytest/fileinpy.py
+++ raytest/fileinpy.py
@@ -1,4 +1,10 @@
 
-file1 = open("myfileinby.txt","w")
+# file1 = open("myfileinby.txt","w")
 
-file1.write("hello semsem\nhow are you")
+# file1.write("hello semsem\nhow are you")
+
+file1 = open("myfileinby.txt","a")
+file1.write("\nnew")
+
+file1 = open("myfileinby.txt","r")
+print(file1.read(24))
