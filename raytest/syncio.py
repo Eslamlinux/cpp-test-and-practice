d78a123458930a6901b459a21ef91d04df736677
Eslamlinux <linuxeslam@gmail.com>
2026-02-17 23:04:43 +0200

    test exception handiling in python

--- raytest/syncio.py
+++ raytest/syncio.py
@@ -1,8 +1,12 @@
 import asyncio
 
 async def func():
-    print("Hi")
+    print("1 sec")
     await asyncio.sleep(1)
-    print("Man")
+    print("2 sec")
+    await asyncio.sleep(2)
+    print("3 sec")
+    await asyncio.sleep(3)
+    print("end")
 
 asyncio.run(func())
