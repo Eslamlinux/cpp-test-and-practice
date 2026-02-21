6d2f64d49e828c0c7d5d5d2deba9918a8cc9178b
Eslamlinux <linuxeslam@gmail.com>
2026-02-21 21:42:56 +0200

    string in python

--- raytest/py15.py
+++ raytest/py15.py
@@ -8,3 +8,8 @@ str2 = "#@#@ i love python @#@#"
 print(str2.strip("#@"))
 print(str2.lstrip("#@"))
 print(str2.rstrip("#@"))
+
+numf1, numf2 ,numf3 = "1","1","11"
+print(numf1.zfill(3))
+print(numf2.zfill(3))
+print(numf3.zfill(3))
