e936d4afdc9e82e3e69c3146c563d21fa3e0be73
Eslamlinux <linuxeslam@gmail.com>
2026-02-21 21:40:17 +0200

    string in python

--- raytest/py15.py
+++ raytest/py15.py
@@ -3,3 +3,8 @@ str = "### i love python ###"
 print(str.strip("#"))
 print(str.lstrip("#"))
 print(str.rstrip("#"))
+
+str2 = "#@#@ i love python @#@#"
+print(str2.strip("#@"))
+print(str2.lstrip("#@"))
+print(str2.rstrip("#@"))
