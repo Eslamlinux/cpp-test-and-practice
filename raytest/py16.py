b3288e555f1547898002ec52ef9642fd5a380185
Eslamlinux <linuxeslam@gmail.com>
2026-02-22 22:58:39 +0200

    string in python

--- raytest/py16.py
+++ raytest/py16.py
@@ -3,3 +3,14 @@ str = "i-love-you"
 print(str.split("-"))
 print(str.split("-",1))
 print(str.rsplit("-",1))
+
+
+str2 = "eslam"
+# print(str2.len())
+# # print(str2.center(str2.len,"*"))
+
+print(str2.startswith("e"))
+
+print(str2.count("e"))
+
+print(str2.swapcase())
