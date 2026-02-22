80a76ba388b37e67a40e872b4ca6c0802bb84180
Eslamlinux <linuxeslam@gmail.com>
2026-02-22 23:01:24 +0200

    string in python

--- raytest/py16.py
+++ raytest/py16.py
@@ -10,6 +10,9 @@ str2 = "eslam"
 # # print(str2.center(str2.len,"*"))
 
 print(str2.startswith("e"))
+print(str2.endswith("m"))
+print(str2.startswith("l",2,5))
+print(str2.endswith("a",2,4))
 
 print(str2.count("e"))
 
