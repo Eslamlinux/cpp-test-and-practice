f5c7aa7364ef0273d94f7d9aaff9b92d810f114f
Eslamlinux <linuxeslam@gmail.com>
2026-03-07 22:55:12 +0200

    string in python

--- pythontest/py18rand.py
+++ pythontest/py18rand.py
@@ -1,9 +1,12 @@
 import random
 
 
-x = 1
-while x > 0:
-    randnum = random.randrange(1,10)
-    print(randnum)
+# x = 1
+# while x > 0:
+#     randnum = random.randrange(1,10)
+#     print(randnum)
     
     
+# s = str(1)
+s = int(1)
+print(type(s))
