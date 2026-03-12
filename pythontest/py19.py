b0d84e5aca5b7d58d73bbeae0ac49ff0f9af71e6
Eslamlinux <linuxeslam@gmail.com>
2026-03-12 16:57:06 +0200

    string in python

--- pythontest/py19.py
+++ pythontest/py19.py
@@ -5,3 +5,9 @@ print(str.casefold()) #لجعل كل الجرف صغيرة
 
 print(str.center(11,"*")) #ازاحة النص بقدر معين 
 
+print(str.count("E")) # عد عدد الرموز في النص
+
+print(str.encode("utf-8"))
+
+print(str.endswith("o"))
+print(str.endswith("L"))
