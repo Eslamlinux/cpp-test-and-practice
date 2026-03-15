ced4721331ac652b0985e8b46e6d92f801227895
Eslamlinux <linuxeslam@gmail.com>
2026-03-16 00:36:58 +0200

    string in python

--- pythontest/py20.py
+++ pythontest/py20.py
@@ -4,3 +4,18 @@ def hello(str):
 
 hello("Eslam")
 hello("mohamed")
+
+# Create variables
+a , b = 15 , 4
+# Print modulus
+print(a%b)
+# Print floor division
+print(a//b)
+
+# Print power
+print(a**b)
+
+# Add 10 to a
+a += 10
+
+print(a,b)
